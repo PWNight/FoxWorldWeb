90862372fe65450913e4e56d57d1ae6b2fc07b32
PWNight <rodionvarzymov@outlook.com>
2024-08-24 00:22:04 +0300

    Implement catch error

--- app/api/v1/guilds/[id]/route.ts
+++ app/api/v1/guilds/[id]/route.ts
@@ -1,11 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import connection from "@/lib/mysql";
-import { useState } from "react";
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
     const id = params.id
-
-    let [guildData] = await connection.promise().query(`SELECT * FROM guilds WHERE id = '${id}'`)
-    console.log(guildData)
-    return NextResponse.json({data: guildData}, {status:200})
+    try{
+        let [guildData] = await connection.promise().query(`SELECT * FROM gauilds WHERE id = '${id}'`)
+        return NextResponse.json({data: guildData}, {status:200})
+    }catch (error: any){
+        return NextResponse.json({status: false, message: 'Internal Server Error', data: {errno: error.errno, sqlState: error.sqlState}}, {status:500})
+    }
 }
