4eb7f1d6e9eca9fb2feadd9abbf98fade9cb31d4
PWNight <rodionvarzymov@outlook.com>
2024-08-24 00:32:55 +0300

    Fix get guild

--- app/api/v1/guilds/[id]/route.ts
+++ app/api/v1/guilds/[id]/route.ts
@@ -4,7 +4,7 @@ import connection from "@/lib/mysql";
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
     const id = params.id
     try{
-        let [guildData] = await connection.promise().query(`SELECT * FROM gauilds WHERE id = '${id}'`)
+        let [guildData] = await connection.promise().query(`SELECT * FROM guilds WHERE id = '${id}'`)
         return NextResponse.json({data: guildData}, {status:200})
     }catch (error: any){
         return NextResponse.json({status: false, message: 'Internal Server Error', data: {errno: error.errno, sqlState: error.sqlState}}, {status:500})
