0508b1f2707b493b046b1f25ab515006d22139da
Найт <rodionvarzymov@outlook.com>
2024-09-12 13:33:39 +0300

    Rewrite main page

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,22 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex sm:min-h-[91vh] min-h-[88vh] flex-col items-center justify-center text-center px-2 py-8">
+    <div className="flex sm:min-h-[91vh] min-h-[88vh] flex-col justify-center px-2 py-8">
+      <div className="flex flex-col m-auto gap-3 text-2xl font-bold mb-4 sm:text-7xl">
       <img className='h-36 w-36 mb-10' src="http://localhost:3000/icon.png"/>
-      <div className="flex flex-row gap-3 text-2xl font-bold mb-4 sm:text-7xl">
         <h1 className="text-[#F38F54]">
           FoxWorld
         </h1>
-      </div>
-      <h2 className="text-xl font-bold mb-4 sm:text-5xl">
+        <h2 className="text-xl font-bold mb-4 sm:text-5xl">
         Наша работа - ваш комфорт
-      </h2>
-      <p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur aliquam porro natus! Officia ex maxime saepe sit ad perferendis temporibus, perspiciatis obcaecati, dignissimos cupiditate quos eaque corporis aliquid neque cum.
-      </p>
-      <div className="flex flex-row items-center gap-5">
+        </h2>
+        <p className="sm:text-xl max-w-[800px] text-muted-foreground">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequuntur aliquam porro natus! Officia ex maxime saepe sit ad perferendis temporibus, perspiciatis obcaecati, dignissimos cupiditate quos eaque corporis aliquid neque cum.
+        </p>
+        <span className="flex flex-row items-center gap-2 text-zinc-400 text-sm mb-7 -mb-12 max-[800px]:mb-12">
+        <Heart className="w-4 h-4 mr-1" /> С любовью к своему делу
+        </span>
+        <div className="flex flex-row items-center gap-5">
         <Link
           href={`https://discord.gg/2yyeWQ5unZ`}
           className={buttonVariants({ variant: "accent", className: "px-6", size: "lg" })}
@@ -35,9 +37,7 @@ export default function Home() {
           Наш Телеграмм канал
         </Link>
       </div>
-      <span className="flex flex-row items-center gap-2 text-zinc-400 text-md mt-7 -mb-12 max-[800px]:mb-12">
-        <Heart className="w-4 h-4 mr-1" /> С любовью к своему делу
-      </span>
+      </div>
     </div>
   );
 }
