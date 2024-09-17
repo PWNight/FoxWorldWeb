f1203ee322500bcdf2b77debd2f5db95c12fb3e4
Найт <rodionvarzymov@outlook.com>
2024-09-17 16:04:21 +0300

    update buttons ui

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,6 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        accent: "bg-[#F38F54] text-primary-foreground hover:bg-[#F38F54]/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
