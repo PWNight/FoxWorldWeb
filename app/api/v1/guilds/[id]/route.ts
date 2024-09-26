3f0514d91ce92db1b10dd14b6bacaec3c13a1acc
PWNight <rodionvarzymov@outlook.com>
2024-09-26 22:37:53 +0300

    Обновил получение всех гильдий

--- app/api/v1/guilds/[id]/route.ts
+++ app/api/v1/guilds/[id]/route.ts
@@ -1,10 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
-import connection from "@/lib/mysql";
+import { getData } from "@/lib/mysql";
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
     const id = params.id
     try{
-        let [guildData] = await connection.promise().query(`SELECT * FROM guilds WHERE id = '${id}'`)
+        let [guildData]:any = await getData(`SELECT * FROM guilds WHERE id = '${id}'`)
+        if(!guildData){
+            return NextResponse.json({message:"Guild not found"}, {status:404});
+        }
         return NextResponse.json({data: guildData}, {status:200})
     }catch (error: any){
         return NextResponse.json({status: false, message: 'Internal Server Error', data: {errno: error.errno, sqlState: error.sqlState}}, {status:500})
