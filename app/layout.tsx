2bc29aa1d1f53d95ad66433ded9a4908ae0440a7
PWNight <rodionvarzymov@outlook.com>
2024-09-26 15:18:05 +0300

    Сделал базовый футер

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 //TODO: Вставить когда напишу футер
-//import { Footer } from "@/components/footer";
+import { Footer } from "@/components/footer";
 
 export default function RootLayout({
   children,
@@ -31,6 +31,7 @@ export default function RootLayout({
           <main className="sm:container mx-auto w-[85vw] h-auto">
             {children}
           </main>
+          <Footer/>
         </ThemeProvider>
       </body>
     </html>
