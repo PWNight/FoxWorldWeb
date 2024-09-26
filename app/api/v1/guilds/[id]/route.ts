363a639bfbccb2230bb958cf6f3863a82941b5c0
PWNight <rodionvarzymov@outlook.com>
2024-09-26 22:41:09 +0300

    Исправил структуру API

--- app/api/v1/guilds/[id]/route.ts
+++ app/api/v1/guilds/[id]/route.ts
@@ -2,13 +2,38 @@ import { NextRequest, NextResponse } from "next/server";
 import { getData } from "@/lib/mysql";
 
 export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-    const id = params.id
     try{
-        let [guildData]:any = await getData(`SELECT * FROM guilds WHERE id = '${id}'`)
+        let [guildData]:any = await getData(`SELECT * FROM guilds WHERE id = '${params.id}'`)
         if(!guildData){
             return NextResponse.json({message:"Guild not found"}, {status:404});
         }
         return NextResponse.json({data: guildData}, {status:200})
+    }catch (error: any){
+        return NextResponse.json({status: false, message: 'Internal Server Error', data: {errno: error.errno, sqlState: error.sqlState}}, {status:500})
+    }
+}
+export async function PATCH(request: NextRequest, { params }: { params: { id: string } }) {
+    const data = await request.json()
+    try{
+        const guildData = await getData(`SELECT * FROM guilds WHERE id = '${params.id}'`)
+        if(!guildData){
+            return NextResponse.json({status: false, message: 'Guild not found'},{status:404})
+        }else{
+            //continue deleting
+        }
+    }catch (error: any){
+        return NextResponse.json({status: false, message: 'Internal Server Error', data: {errno: error.errno, sqlState: error.sqlState}}, {status:500})
+    }
+}
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+    const data = await request.json()
+    try{
+        const guildData = await getData(`SELECT * FROM guilds WHERE id = '${params.id}'`)
+        if(!guildData){
+            return NextResponse.json({status: false, message: 'Guild not found'},{status:404})
+        }else{
+            //continue deleting
+        }
     }catch (error: any){
         return NextResponse.json({status: false, message: 'Internal Server Error', data: {errno: error.errno, sqlState: error.sqlState}}, {status:500})
     }
--- app/api/v1/guilds/route.ts
+++ app/api/v1/guilds/route.ts
@@ -25,29 +25,3 @@ export async function POST(request: NextRequest) {
         return NextResponse.json({status: false, message: 'Internal Server Error', data: {errno: error.errno, sqlState: error.sqlState}}, {status:500})
     }
 }
-export async function DELETE(request: NextRequest) {
-    const params = await request.json()
-    try{
-        const guildData = await getData(`SELECT * FROM guilds WHERE id = '${params.id}'`)
-        if(!guildData){
-            return NextResponse.json({status: false, message: 'Guild not found'},{status:404})
-        }else{
-            //continue deleting
-        }
-    }catch (error: any){
-        return NextResponse.json({status: false, message: 'Internal Server Error', data: {errno: error.errno, sqlState: error.sqlState}}, {status:500})
-    }
-}
-export async function PATCH(request: NextRequest) {
-    const params = await request.json()
-    try{
-        const guildData = await getData(`SELECT * FROM guilds WHERE id = '${params.id}'`)
-        if(!guildData){
-            return NextResponse.json({status: false, message: 'Guild not found'},{status:404})
-        }else{
-            //continue deleting
-        }
-    }catch (error: any){
-        return NextResponse.json({status: false, message: 'Internal Server Error', data: {errno: error.errno, sqlState: error.sqlState}}, {status:500})
-    }
-}
