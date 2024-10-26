8481ba24b44d5ec885adb5f855d3fa9c1e5a223e
PWNight <rodionvarzymov@outlook.com>
2024-10-27 01:44:13 +0300

    Исправил цвет текста на кнопках

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
-        accent: "bg-[#F38F54] text-primary-foreground hover:bg-[#F38F54]/90",
+        accent: "bg-orange-400 text-white hover:bg-orange-500",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
