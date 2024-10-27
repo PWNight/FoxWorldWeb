767af77078af87422031acb2747c730c08d323d3
PWNight <rodionvarzymov@outlook.com>
2024-10-28 01:21:38 +0300

    Сделал макет для кнопки ЛК в навигации

--- components/account-button.tsx
+++ components/account-button.tsx
@@ -30,12 +30,20 @@ export function AccountButton() {
             <DropdownMenu>
                 <DropdownMenuTrigger asChild>
                     <Button variant="ghost" size="icon">
-                    <img src={"https://mineskin.eu/helm/"+userData.last_nickname}></img>
+                        <img className='rounded-lg' src={"https://mineskin.eu/helm/"+userData.last_nickname}></img>
                     </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="end" className="p-4 mt-2 py-10 flex flex-col gap-10 rounded-lg">
                     <DropdownMenuItem className="text-xl">
-                        <h1>{userData.last_nickname}</h1>
+                        <div className="flex items-center gap-5">
+                            <div className="w-14 h-14">
+                                <img className='rounded-lg' src={"https://mineskin.eu/helm/"+userData.last_nickname}></img>
+                            </div>
+                            <div className="flex flex-col text-lg">
+                                <h1 className="text-2xl">{userData.last_nickname}</h1>
+                                <p>Без доступа</p>
+                            </div>
+                        </div>
                     </DropdownMenuItem>
                     <div>
                         <DropdownMenuItem onClick={() => router.push('/me')} className="text-xl">
