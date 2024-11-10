d6c01a140131ce710bbc992b52e132df335e8a7f
Найт <rodionvarzymov@outlook.com>
2024-11-10 14:21:20 +0300

    Начал писать страницу ЛК

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -1,6 +1,9 @@
 "use client"
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import SubAnchor from "@/components/subanchor";
 
 export default function Me() {
     const [userData, setUserData] = useState(Object)
@@ -14,13 +17,41 @@ export default function Me() {
             return
         }
         const json = await response.json()
-        setUserData(json.data.data)
+        setUserData(json)
     }
     useEffect(()=>{
         getSession()
     },[])
     return (
-        <>
-        </>
+        <div className="sm:container grid grid-cols-3 mt-6">
+            <div className="flex items-center">
+                <div className="bg-neutral-100 dark:bg-neutral-900 rounded-md flex flex-col gap-5 items-center px-4 py-2 lg:min-w-[250px]">
+                    <h1 className="border-b text-xl">Меню навигации</h1>
+                    <div className="w-full flex flex-col gap-2 items-center">
+                        <SubAnchor
+                            key='/me'
+                            activeClassName="bg-orange-300"
+                            absolute
+                            className="flex items-center gap-1 hover:bg-orange-200 transition-all w-full py-2 px-1 rounded-sm"
+                            href='/me'
+                        >
+                        Главная страница
+                        </SubAnchor>
+                        <SubAnchor
+                            key='/me/security'
+                            activeClassName="bg-orange-300"
+                            absolute
+                            className="flex items-center gap-1 hover:bg-orange-200 transition-all w-full py-2 px-1 rounded-sm"
+                            href='/me/security'
+                        >
+                        Настройки безопасности
+                        </SubAnchor>
+                    </div>
+                </div>
+            </div>
+            <div className="col-span-2">
+                <p>t</p>
+            </div>
+        </div>
     )
 }
