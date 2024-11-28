2426e57917cac6cc4b3f149ea19188d89f491bf2
PWNight <rodionvarzymov@outlook.com>
2024-11-28 12:54:14 +0300

    Новый дизайн страницы новостей

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,29 +1,31 @@
-import { buttonVariants } from "@/components/ui/button";
-import { NewsMdxFrontmatter, getAllNews } from "@/lib/markdown";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Author, NewsMdxFrontmatter, getAllNews } from "@/lib/markdown";
 import { formatDate2, stringToDate } from "@/lib/utils";
 import { Metadata } from "next";
+import Image from "next/image";
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "FoxWorld - Новости",
+  title: "AriaDocs - news",
 };
 
 export default async function NewsIndexPage() {
   const news = (await getAllNews()).sort(
-    (a, b) =>
-      stringToDate(b.frontmatter.date).getTime() -
-      stringToDate(a.frontmatter.date).getTime()
+    (a, b) => stringToDate(b.date).getTime() - stringToDate(a.date).getTime()
   );
   return (
-    <div className="w-full flex flex-col gap-1 pt-2">
-      <div className="mb-6 flex flex-col gap-2 ">
+    <div className="w-full mx-auto flex flex-col gap-1 sm:min-h-[91vh] min-h-[88vh] pt-2">
+      <div className="mb-7 flex flex-col gap-2">
         <h1 className="text-3xl font-extrabold">
-          Новости сервера
+          The latest news of this product
         </h1>
+        <p className="text-muted-foreground">
+          All the latest news and news, straight from the team.
+        </p>
       </div>
-      <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
+      <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-8 gap-4 mb-5">
         {news.map((news) => (
-          <NewsCard {...news.frontmatter} slug={news.slug} key={news.slug} />
+          <NewsCard {...news} slug={news.slug} key={news.slug} />
         ))}
       </div>
     </div>
@@ -31,40 +33,64 @@ export default async function NewsIndexPage() {
 }
 
 function NewsCard({
-  imageUrl,
-  imageAlt,
   date,
   title,
   description,
   slug,
+  cover,
+  authors,
 }: NewsMdxFrontmatter & { slug: string }) {
   return (
-    <div className="grid content-between gap-2 items-start border rounded-md">
-      <div className="m-auto">
-        <img src={imageUrl} alt={imageAlt}/>
+    <Link
+      href={`/news/${slug}`}
+      className="flex flex-col gap-2 items-start border rounded-md py-5 px-3 min-h-[400px]"
+    >
+      <h3 className="text-md font-semibold -mt-1 pr-7">{title}</h3>
+      <div className="w-full">
+        <Image
+          src={cover}
+          alt={title}
+          width={400}
+          height={150}
+          quality={80}
+          className="w-full rounded-md object-cover h-[180px] border"
+        />
       </div>
-      <div className="p-5 pt-7 flex flex-col gap-2">
-        <Link
-          href={`/news/${slug}`}
-          className="sm:text-lg text-lg font-semibold -mt-1"
-        >
-          {title}
-        </Link>
-        <p className="text-sm text-muted-foreground">{description}</p>
-        <p className="text-[13px] text-muted-foreground mb-1">
-          Опубликовано {formatDate2(date)}
+      <p className="text-sm text-muted-foreground">{description}</p>
+      <div className="flex items-center justify-between w-full mt-auto">
+        <p className="text-[13px] text-muted-foreground">
+          Published on {formatDate2(date)}
         </p>
-        <Link
-          href={`/news/${slug}`}
-          className={buttonVariants({
-            className: "w-full mt-auto",
-            variant: "secondary",
-            size: "sm",
-          })}
-        >
-          Читать
-        </Link>
+        <AvatarGroup users={authors} />
       </div>
+    </Link>
+  );
+}
+
+function AvatarGroup({ users, max = 4 }: { users: Author[]; max?: number }) {
+  const displayUsers = users.slice(0, max);
+  const remainingUsers = Math.max(users.length - max, 0);
+
+  return (
+    <div className="flex items-center">
+      {displayUsers.map((user, index) => (
+        <Avatar
+          key={user.username}
+          className={`inline-block border-2 w-9 h-9 border-background ${
+            index !== 0 ? "-ml-3" : ""
+          } `}
+        >
+          <AvatarImage src={user.avatar} alt={user.username} />
+          <AvatarFallback>
+            {user.username.slice(0, 2).toUpperCase()}
+          </AvatarFallback>
+        </Avatar>
+      ))}
+      {remainingUsers > 0 && (
+        <Avatar className="-ml-3 inline-block border-2 border-background hover:translate-y-1 transition-transform">
+          <AvatarFallback>+{remainingUsers}</AvatarFallback>
+        </Avatar>
+      )}
     </div>
   );
 }
