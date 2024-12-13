2ab2c6d0cf5a49b39537542b00e37a1e2435a840
Найт <rodionvarzymov@outlook.com>
2024-12-13 14:25:12 +0300

    Исправил качество фото в новости

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -50,8 +50,8 @@ export default async function NewsPage({ params: { slug } }: PageProps) {
             alt={res.frontmatter.title}
             width={400}
             height={250}
-            quality={80}
-            className="w-full rounded-md object-cover h-[250px] border"
+            quality={100}
+            className="rounded-md object-cover border"
           />
         </div>
         <p className="text-muted-foreground text-sm">
