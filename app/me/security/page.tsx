850fde9b3dc907b87c9fe069fcf17eb1cec3f0c9
PWNight <rodionvarzymov@outlook.com>
2024-12-31 16:00:08 +0300

    Написал прототип информации о сессиях

--- app/me/security/page.tsx
+++ app/me/security/page.tsx
@@ -5,9 +5,11 @@ import {Pencil} from "lucide-react";
 import Link from "next/link";
 import {Button} from "@/components/ui/button";
 import {NavMe} from "@/components/navbar_me";
+import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from "@/components/ui/accordion";
 
 export default function MeSecurity() {
     const [userData, setUserData] = useState(Object)
+    const [statsData, setStatsData] = useState(Object)
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
@@ -19,6 +21,20 @@ export default function MeSecurity() {
     const router = useRouter()
 
     useEffect(()=>{
+        async function getStats(data : any){
+            const response = await fetch(`http://135.181.126.159:25576/v1/player?player=${data.profile.fk_uuid}`,{
+                method: "GET"
+            })
+            if(!response.ok){
+                // TODO: Implement error handler
+                console.log(response)
+                return
+            }
+            const json = await response.json()
+            const sessions = json.sessions.slice(0,5)
+            console.log(sessions)
+            setStatsData(sessions)
+        }
         async function getSession(){
             const response = await fetch("/api/v1/users/me",{
                 method: "GET"
@@ -34,7 +50,7 @@ export default function MeSecurity() {
                 router.push('/login')
             }else{
                 setUserData(json)
-                console.log(json)
+                getStats(json)
             }
         }
         getSession()
@@ -113,7 +129,7 @@ export default function MeSecurity() {
         if (!hasError) {
             const result = await fetch('/api/v1/auth/update-data',{
                 method: 'POST',
-                body: JSON.stringify({action:'change_password',old_value:'None',new_value:password})
+                body: JSON.stringify({action:'change_password',old_value:'None',new_value:password, user_data: userData})
             })
             const json : any = await result.json()
             console.log(json)
@@ -125,7 +141,7 @@ export default function MeSecurity() {
         }
     };
 
-    if(Object.keys(userData).length != 0){
+    if(Object.keys(userData).length != 0 && Object.keys(statsData).length != 0){
         return (
             <div className="grid sm:grid-cols-[300px,1fr] gap-6 mt-6">
                 <NavMe/>
@@ -156,8 +172,7 @@ export default function MeSecurity() {
                                 {usernameError && <p className="text-red-400 mt-1">{usernameError}</p>}
                             </div>
                         </div>
-                        <div
-                            className="bg-neutral-100 rounded-sm p-4 max-h-fit flex justify-center flex-col dark:bg-neutral-800">
+                        <div className="bg-neutral-100 rounded-sm p-4 max-h-fit flex justify-center flex-col dark:bg-neutral-800">
                             <div className="border-b">
                                 <h1 className="text-2xl">Изменение пароля</h1>
                             </div>
@@ -175,14 +190,38 @@ export default function MeSecurity() {
                                     <Button variant="accent" className="flex gap-1"><Pencil/>Изменить</Button>
                                 </form>
                                 {passwordError && <p className="text-red-400 mt-1">{passwordError}</p>}
-                                <Link href='/recover'
+                                <Link href='/  '
                                       className="text-orange-400 hover:text-orange-500 transition-all 2xl:flex gap-2">
                                     <p className="text-muted-foreground">Забыли пароль?</p>Перейти к восстановлению
                                 </Link>
                             </div>
                         </div>
                     </div>
-                    <div className="bg-neutral-100 rounded-sm p-4 dark:bg-neutral-800 h-full">
+                    <div className="h-full">
+                        <div className="bg-neutral-100 rounded-sm p-4 max-h-fit dark:bg-neutral-800 mb-4">
+                            <h1 className="text-2xl">Последние 5 сессий</h1>
+                        </div>
+                        <Accordion type='multiple' className="flex flex-col">
+                            {
+                                statsData.map((session: any, key: any) => (
+                                    <AccordionItem value={session.start} key={key}>
+                                        <AccordionTrigger className='border-b bg-neutral-100 rounded-sm p-4 max-h-fit dark:bg-neutral-800 mb-1 w-full'>
+                                            <h1 className="text-2xl">Сессия от {session.start}</h1>
+                                        </AccordionTrigger>
+                                        <AccordionContent
+                                            className="bg-neutral-100 rounded-sm p-4 max-h-fit dark:bg-neutral-800">
+                                            <div className="flex gap-1"><p className="text-muted-foreground">Время сессии:</p><p>{session.start} - {session.end}</p></div>
+                                            <div className="flex gap-1"><p className="text-muted-foreground">Сессия
+                                                длилась</p><p>{session.length}</p></div>
+                                            <div className="flex gap-1"><p className="text-muted-foreground">Большую часть времени вы были в мире</p><p>{session.most_used_world}</p></div>
+                                            <div className="flex gap-1"><p className="text-muted-foreground">Вы
+                                                провели</p><p>{session.afk_time}</p><p
+                                                className="text-muted-foreground">в АФК</p></div>
+                                        </AccordionContent>
+                                    </AccordionItem>
+                                ))
+                            }
+                        </Accordion>
                     </div>
                 </div>
             </div>
