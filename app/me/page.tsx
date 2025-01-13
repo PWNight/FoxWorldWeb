1eb62b6c37e290bb5e70efafaa35db07cc6e7c96
Найт <rodionvarzymov@outlook.com>
2025-01-13 16:02:11 +0300

    Исправил баг блока статистики

--- app/me/page.tsx
+++ app/me/page.tsx
@@ -79,7 +79,7 @@ export default function Me() {
                             </div>
                         </div>
                     </div>
-                    <div className="grid lg:grid-rows-2 xl:grid-cols-3">
+                    <div className="">
                         <div className="bg-neutral-100 rounded-sm p-4 lg:row-span-1 lg:col-span-2 dark:bg-neutral-800">
                             <div className="border-b">
                                 <h1 className="text-2xl">Игровая статистика</h1>
