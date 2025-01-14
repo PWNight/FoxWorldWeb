3d141467afc28f8cc03769426d04c12e28af6846
Найт <rodionvarzymov@outlook.com>
2025-01-14 16:02:42 +0300

    Добавил карту сервера

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -3,6 +3,6 @@ import InDev from "@/components/indev";
 
 export default function Map() {
     return (
-        <InDev/>
+        <iframe src='http://mc.foxworld.ru:25599/' className='w-full h-full'></iframe>
     );
 }
