dddd060652a38b6d4c3e895bbf548c80c1634a97
Найт <rodionvarzymov@outlook.com>
2025-01-18 15:05:49 +0300

    Фикс футера

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,11 +5,11 @@ export function Footer() {
   return (
     <footer className="border-t w-full p-5 max-h-14">
       <div className="container flex items-center sm:justify-between justify-center sm:gap-0 gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3 max-sm:px-4">
-          <div className="flex items-center gap-3 flex-col">
-              <p className="text-center flex flex-row items-center">
+          <div className="flex flex-col">
+              <p>
                   FoxWorld © 2021 - 2024
               </p>
-              <p className="text-center flex flex-row items-center">
+              <p>
                   v1.0.0-beta1 (In DEV)
               </p>
           </div>
