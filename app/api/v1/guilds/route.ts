f8a2aa93bab55cc699c5ceba8b47be0febeed569
Найт <rodionvarzymov@outlook.com>
2025-01-18 19:45:45 +0300

    Исправил формат ответов API гильдий

--- app/api/v1/guilds/route.ts
+++ app/api/v1/guilds/route.ts
@@ -6,11 +6,11 @@ export async function GET(request: NextRequest) {
     try{
         let guildData:any = await query("SELECT * FROM guilds")
         if(!guildData){
-            return NextResponse.json({message: "Guilds not found"}, {status:404})
+            return NextResponse.json({success: true, data: {}}, {status:404})
         }
-        return NextResponse.json({data: guildData}, {status:200})
+        return NextResponse.json({success: true, data: guildData}, {status:200})
     }catch (error: any){
-        return NextResponse.json({status: false, message: 'Internal Server Error', data: {errno: error.errno, sqlState: error.sqlState}}, {status:500})
+        return NextResponse.json({success: false, message: 'Internal Server Error', data: {errno: error.errno, sqlState: error.sqlState}}, {status:500})
     }
 }
 export async function POST(request: NextRequest) {
@@ -36,12 +36,12 @@ export async function POST(request: NextRequest) {
         try{
             const userGuilds : any = await query('SELECT permission FROM guilds_members WHERE uid = ?', [user_id])
             if (userGuilds.length > 0 && userGuilds[0].permission == 2){
-                return NextResponse.json({status: false, message: 'Вы уже являетесь главой одной из гильдий'},{status:401})
+                return NextResponse.json({success: false, message: 'Вы уже являетесь главой одной из гильдий'},{status:401})
             }
 
             const guildData : any = await query('SELECT * FROM guilds WHERE url = ?', [url])
             if(guildData.length != 0){
-                return NextResponse.json({status: false, message: 'Гильдия с данной ссылкой уже существует'},{status:500})
+                return NextResponse.json({success: false, message: 'Гильдия с данной ссылкой уже существует'},{status:500})
             }
 
             await query('INSERT INTO guilds (owner_id, url, name, description, info) VALUES (?, ?, ?, ?, ?) RETURNING id', [user_id, url, name, description, info]);
@@ -49,9 +49,9 @@ export async function POST(request: NextRequest) {
             const guild : any = await query("SELECT id FROM guilds WHERE url = ?", [url])
             await query('INSERT INTO guilds_members (fk_guild, uid, permission) VALUES (?, ?, ?)', [guild[0].id, user_id, 2])
 
-            return NextResponse.json({status: true, message: 'Гильдия успешно создана'},{status:200})
+            return NextResponse.json({success: true, message: 'Гильдия успешно создана'},{status:200})
         }catch (error: any){
-            return NextResponse.json({status: false, message: 'Internal Server Error', data: {errno: error.errno, sqlState: error.sqlState}}, {status:500})
+            return NextResponse.json({success: false, message: 'Internal Server Error', data: {errno: error.errno, sqlState: error.sqlState}}, {status:500})
         }
     }
 }
