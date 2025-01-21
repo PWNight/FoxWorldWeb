a1205427691d642d6b1128ccc978179cb532cf97
PWNight <rodionvarzymov@outlook.com>
2025-01-21 23:20:31 +0300

    Накидал дизайн для управления гильдией

--- app/me/guilds/[url]/page.tsx
+++ app/me/guilds/[url]/page.tsx
@@ -2,7 +2,9 @@
 import { useRouter } from"next/navigation";
 import { useEffect, useState } from"react";
 import {NavMe} from "@/components/navbar_me";
-import {LucideLoader} from "lucide-react";
+import {CloudUpload, LucideLoader, Trash2} from "lucide-react";
+import Link from "next/link";
+import {Button} from "@/components/ui/button";
 
 type PageProps = {
     params: Promise<{ url: string }>;
@@ -90,7 +92,7 @@ export default function MyGuild(props: PageProps) {
         const json : any = await result.json()
         if(json.success){
             setIsLoading(false)
-            //continue
+            router.push('/me/guilds')
         }else{
             setIsLoading(false);
             setError(json.message)
@@ -112,7 +114,6 @@ export default function MyGuild(props: PageProps) {
                                 type="text"
                                 id="name"
                                 placeholder='Введите новое название'
-                                value={updateFormData.value}
                                 onChange={(e) => setUpdateFormData({name: e.target.id, value: e.target.value})}
                                 className="bg-gray-50 border border-gray-300 rounded-lg focus:ring-orange-300 focus:border-orange-400 block w-full p-2.5 dark:bg-neutral-800 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-orange-300 dark:focus:border-orange-400"
                             />
@@ -123,12 +124,11 @@ export default function MyGuild(props: PageProps) {
                             {error && <p className="text-red-400 mt-1 mb-5">{error}</p>}
                         </form>
                         <form className="mb-5 select-none gap-2 flex flex-col" onSubmit={handleUpdate}>
-                            <label htmlFor="url" className="block mb-2 font-medium">Краткая ссылка: <b>/{userGuild.url}</b></label>
+                            <label htmlFor="url" className="block mb-2 font-medium">Краткая ссылка на гильдию: <b>/{userGuild.url}</b></label>
                             <input
                                 type="text"
                                 id="url"
                                 placeholder='Введите новую ссылку'
-                                value={updateFormData.value}
                                 onChange={(e) => setUpdateFormData({name: e.target.id, value: e.target.value})}
                                 className="bg-gray-50 border border-gray-300 rounded-lg focus:ring-orange-300 focus:border-orange-400 block w-full p-2.5 dark:bg-neutral-800 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-orange-300 dark:focus:border-orange-400"
                             />
@@ -139,12 +139,11 @@ export default function MyGuild(props: PageProps) {
                             {error && <p className="text-red-400 mt-1 mb-5">{error}</p>}
                         </form>
                         <form className="mb-5 select-none gap-2 flex flex-col" onSubmit={handleUpdate}>
-                            <label htmlFor="info" className="block mb-2 font-medium">Слоган: <b>{userGuild.info}</b></label>
+                            <label htmlFor="info" className="block mb-2 font-medium">Слоган вашей гильдии: <b>{userGuild.info}</b></label>
                             <input
                                 type="text"
                                 id="info"
                                 placeholder='Введите новый слоган'
-                                value={updateFormData.value}
                                 onChange={(e) => setUpdateFormData({name: e.target.id, value: e.target.value})}
                                 className="bg-gray-50 border border-gray-300 rounded-lg focus:ring-orange-300 focus:border-orange-400 block w-full p-2.5 dark:bg-neutral-800 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-orange-300 dark:focus:border-orange-400"
                             />
@@ -155,12 +154,11 @@ export default function MyGuild(props: PageProps) {
                             {error && <p className="text-red-400 mt-1 mb-5">{error}</p>}
                         </form>
                         <form className="mb-5 select-none gap-2 flex flex-col" onSubmit={handleUpdate}>
-                            <label htmlFor="description" className="block mb-2 font-medium">Полное описание: <b>{userGuild.description}</b></label>
+                            <label htmlFor="description" className="block mb-2 font-medium">Полное описание вашей гильдии: <b>{userGuild.description}</b></label>
                             <input
                                 type="text"
                                 id="description"
                                 placeholder='Введите новое описание'
-                                value={updateFormData.value}
                                 onChange={(e) => setUpdateFormData({name: e.target.id, value: e.target.value})}
                                 className="bg-gray-50 border border-gray-300 rounded-lg focus:ring-orange-300 focus:border-orange-400 block w-full p-2.5 dark:bg-neutral-800 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-orange-300 dark:focus:border-orange-400"
                             />
@@ -174,7 +172,60 @@ export default function MyGuild(props: PageProps) {
                 </div>
                 <div className="flex flex-col gap-2 ">
                     <div
-                        className="bg-neutral-100 rounded-sm p-4 flex justify-center flex-col dark:bg-neutral-800 h-full">
+                        className="bg-neutral-100 rounded-sm p-4 max-h-fit flex justify-center flex-col dark:bg-neutral-800">
+                        <div className="border-b">
+                            <h1 className="text-2xl">Эмблема гильдии (В разработке)</h1>
+                            <p className="text-muted-foreground">Здесь вы можете изменить эмблему гильдии</p>
+                        </div>
+                        <div className="flex flex-col gap-4 my-2">
+                            <Link href='/wiki/rules' className="text-orange-400 hover:text-orange-500 transition-all"><p
+                                className="text-muted-foreground">Устанавливаемая эмблема не должен нарушать</p>правила
+                                сервера</Link>
+                            <div className="flex 2xl:flex-row flex-col gap-2">
+                                <Button variant='accent' className="flex gap-1"><CloudUpload/>Выбрать файл</Button>
+                                <Button variant='destructive' className="flex gap-1"><Trash2/>Удалить эмблему</Button>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        className="bg-neutral-100 rounded-sm p-4 max-h-fit flex justify-center flex-col dark:bg-neutral-800">
+                        <div className="border-b">
+                            <h1 className="text-2xl">Дискорд сервер гильдии (В разработке)</h1>
+                            <p className="text-muted-foreground">Здесь вы можете указать ссылку на дискрод сервер вашей
+                                гильдии</p>
+                        </div>
+                        <div className="flex flex-col my-4">
+                            <form className="mb-5 select-none gap-2 flex flex-col xl:flex-row" onSubmit={handleUpdate}>
+                                <input
+                                    type="text"
+                                    id="description"
+                                    placeholder='Введите ссылку на сервер'
+                                    onChange={(e) => setUpdateFormData({name: e.target.id, value: e.target.value})}
+                                    className="bg-gray-50 border border-gray-300 rounded-lg focus:ring-orange-300 focus:border-orange-400 block w-full p-2.5 dark:bg-neutral-800 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-orange-300 dark:focus:border-orange-400"
+                                />
+                                <button type="submit"
+                                        disabled={isLoading}
+                                        className="select-none text-white bg-orange-400 hover:bg-orange-500 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center flex items-center gap-1">{isLoading ? <>
+                                    <LucideLoader/><p>Выполняю..</p></> : 'Изменить данные'}</button>
+                                {error && <p className="text-red-400 mt-1 mb-5">{error}</p>}
+                            </form>
+                            <Button variant='destructive' className="flex gap-1"><Trash2/>Удалить сервер</Button>
+                        </div>
+                    </div>
+                    <div
+                        className="bg-neutral-100 rounded-sm p-4 max-h-fit flex justify-center flex-col dark:bg-neutral-800">
+                        <div className="border-b">
+                            <h1 className="text-2xl">Статус входа в гильдию (В разработке)</h1>
+                            <p className="text-muted-foreground">Здесь вы можете временно отключить возможность <br/>подавать заявку в гильдию и наоборот</p>
+                        </div>
+                        <div className="flex xl:flex-row flex-col my-4 gap-2">
+                            <Button variant='accent' className="flex gap-1">Разрешить заявки</Button>
+                            <Button variant='destructive' className="flex gap-1">Запретить заявки</Button>
+                        </div>
+                    </div>
+                    <div className="flex xl:flex-row flex-col gap-4">
+                        <Button variant='accent' className="flex gap-1 w-full">Управление участниками</Button>
+                        <Button variant='destructive' className="flex gap-1 w-full">Удалить гильдию</Button>
                     </div>
                 </div>
             </div>
