4ee76a57c66be8e7f4df2c3cf37eb7b1b7abc959
Найт <rodionvarzymov@outlook.com>
2025-01-25 23:56:52 +0300

    Улучшил код API users/me

--- app/api/v1/users/me/route.ts
+++ app/api/v1/users/me/route.ts
@@ -1,6 +1,7 @@
 import { query } from "@/lib/mysql";
 import { decrypt } from "@/lib/session";
 import { NextRequest, NextResponse } from "next/server";
+import Joi from "joi";
 
 async function checkToken(token: any){
     const data:any = await decrypt(token)
@@ -11,51 +12,42 @@ async function checkToken(token: any){
 
     try{
         const {uuid} = data.data;
-        let profiles : any = await query('SELECT * FROM profiles WHERE fk_uuid = ?', [uuid])
 
-        if (profiles.length === 0) {
+        let [profile] : any = await query('SELECT * FROM profiles WHERE fk_uuid = ?', [uuid])
+        if ( !profile ) {
             return NextResponse.json({ success: false, message: 'Пользователь не найден, обратитесь к администратору площадки' }, { status: 404 });
         }
-        const profile = profiles[0];
 
-        const users : any = await query('SELECT * FROM librepremium_data WHERE uuid = ?', [profile.fk_uuid])
-        const user = users[0];
+        const [user] : any = await query('SELECT * FROM librepremium_data WHERE uuid = ?', [profile.fk_uuid])
+        if ( !user ) {
+            return NextResponse.json({ success: false, message: 'Пользователь не найден, обратитесь к администратору площадки' }, { status: 404 });
+        }
+
         const { premium_uuid, joined, last_seen } = user;
-
-        return NextResponse.json({success: true, user: {premium_uuid, joined, last_seen}, profile, token }, {status:200})
+        return NextResponse.json({ success: true, user: {premium_uuid, joined, last_seen}, profile, token }, {status:200})
     }catch (error: any){
-        return NextResponse.json({success: false, message: 'Internal Server Error', error}, {status:500})
+        return NextResponse.json({ success: false, message: 'Internal Server Error', error }, {status:500})
     }
 }
 
 export async function GET(request: NextRequest) {
     const session_token = request.cookies.get('s_token')
-    if(session_token === undefined){
-        return NextResponse.json({success: false, message: "Отсутсвует токен сессии в куки браузера"}, { status: 401 })
+    if( session_token === undefined ){
+        return NextResponse.json({ success: false, message: "Отсутсвует токен сессии в куки браузера" }, { status: 401 })
     }
-
     return await checkToken(session_token?.value)
 }
 export async function POST(request: NextRequest) {
-    const data:any = await request.json()
-    const session_token = data.session_token
+    const data = await request.json();
+    const session_token = data.token;
 
-    let hasErrors = false;
-    let error = ''
+    const userSchema = Joi.object({
+        session_token: Joi.string().required(),
+    })
 
-    if (session_token == null || session_token === '' || typeof session_token !== 'string') {
-        hasErrors = true;
-        if (session_token == null || session_token === '') {
-            error = 'Токен сессии отсутствует';
-        } else {
-            if (typeof session_token !== 'string') {
-                error = 'Токен сессии должен быть строкой';
-            }
-        }
-    }
-
-    if (hasErrors) {
-        return NextResponse.json({ success: false, error }, { status: 401 });
+    const { error } = userSchema.validate(data);
+    if ( error ) {
+        return NextResponse.json({success: false, message: "Отсутствуют некоторые параметры", error}, {status: 401});
     }
 
     return await checkToken(session_token)
