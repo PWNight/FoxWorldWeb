6c4d81920fee1e139a20c44e620eed394c14a753
Найт <rodionvarzymov@outlook.com>
2025-01-26 00:00:54 +0300

    Улучшил код API users/me/change-username

--- app/api/v1/users/me/change-username/route.ts
+++ app/api/v1/users/me/change-username/route.ts
@@ -1,57 +1,35 @@
 import { query } from '@/lib/mysql';
 import { rconQuery } from '@/lib/rcon'
 import { NextRequest, NextResponse } from "next/server";
+import Joi from "joi";
 
 export async function POST(request: NextRequest) {
-    let hasErrors = false;
-    let errors = {
-        session_token: '',
-        new_username: '',
-    };
-
     const data = await request.json();
-    const session_token = data.session_token;
+    const session_token = data.token;
     const new_username = data.new_username;
 
-    if (session_token == null || session_token === '' || typeof session_token !== 'string') {
-        hasErrors = true;
-        if (session_token == null || session_token === '') {
-            errors.session_token = 'Токен сессии отсутствует';
-        } else {
-            if (typeof session_token !== 'string') {
-                errors.session_token = 'Токен сессии должен быть строкой';
-            }
-        }
-    }
+    const userSchema = Joi.object({
+        session_token: Joi.string().required(),
+        new_username: Joi.string().required(),
+    })
 
-    if (new_username == null || new_username === '' || typeof new_username !== 'string') {
-        hasErrors = true;
-        if (new_username == null || new_username === '') {
-            errors.new_username = 'Новый никнейм отсутствует';
-        } else {
-            if (typeof new_username !== 'string') {
-                errors.new_username = 'Новый никнейм должен быть строкой';
-            }
-        }
-    }
-
-    if (hasErrors) {
-        return NextResponse.json({ success: false, message: "Ошибка валидации", errors }, { status: 401 });
+    const { error } = userSchema.validate(data);
+    if ( error ) {
+        return NextResponse.json({success: false, message: "Отсутствуют некоторые параметры", error}, {status: 401});
     }
 
     let response = await fetch("https://foxworld.ru/api/v1/users/me",{
         method: "POST",
         body: JSON.stringify({session_token}),
     })
-
     const json = await response.json()
-    if(!json.success){
+    if( !json.success ){
         return NextResponse.json({ success: false, message: "Не удалось получить данные сессии" }, { status: 401 });
     }
 
     // Получение пользователя из базы данных
-    const libre_user: any = await query('SELECT * FROM librepremium_data WHERE last_nickname = ?', [new_username]);
-    if (libre_user.length != 0) {
+    const [libre_user] : any = await query('SELECT * FROM librepremium_data WHERE last_nickname = ?', [new_username]);
+    if ( !libre_user ) {
         return NextResponse.json({ success: false, message: 'Данный никнейм уже занят' }, { status: 403 });
     }
 
