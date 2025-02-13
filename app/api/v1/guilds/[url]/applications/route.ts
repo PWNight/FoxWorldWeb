707d50c6e30616ca9803b0c440d81910ec0ed8a5
Найт <rodionvarzymov@outlook.com>
2025-02-13 09:01:54 +0300

    Доработал API получения заявок в гильдию

--- app/api/v1/guilds/[url]/applications/route.ts
+++ app/api/v1/guilds/[url]/applications/route.ts
@@ -8,23 +8,11 @@ const applicationSchema = Joi.object({
 })
 
 export async function GET(request: NextRequest, {params}: { params: Promise<{ url: string }> }) {
-
-}
-
-export async function POST(request: NextRequest, {params}: { params: Promise<{ url: string }> }) {
     const {url} = await params;
-    const data = await request.json();
-
-    const { error } = applicationSchema.validate(data);
-    if (error) {
-        return NextResponse.json({ success: false, message: "Ошибка валидации", error: error.details }, { status: 400 });
-    }
-
-    const { about_user, why_this_guild } = data;
 
     const authHeader = request.headers.get("authorization");
     if (!authHeader) {
-        return NextResponse.json({success: false, message: "Отсутствует заголовок авторизации"},{status: 401})
+        return NextResponse.json({success: false, message: 'Отсутствует заголовок авторизации'},{status: 401})
     }
     const token = authHeader.split(" ")[1];
 
@@ -36,7 +24,59 @@ export async function POST(request: NextRequest, {params}: { params: Promise<{ u
 
         if ( !response.ok ){
             const errorData = await response.json()
-            return NextResponse.json({success: false, message: "Не удалось получить данные о пользователе", error: errorData || response.statusText},{status: response.status})
+            return NextResponse.json({success: false, message: 'Не удалось получить данные о пользователе', error: errorData || response.statusText},{status: response.status})
+        }
+
+        const json = await response.json()
+        if( !json.success ){
+            return NextResponse.json({ success: false, message: json.message }, { status: 401 });
+        }
+
+        const user = json.profile;
+
+        const [guildData] : any = await query(`SELECT * FROM guilds WHERE url = ?`, [url])
+        if( !guildData ){
+            return NextResponse.json({ success: false, message: 'Гильдия не найдена' }, { status: 400 })
+        }
+
+        const [guildAccess] : any = await query(`SELECT permission FROM guilds_members WHERE fk_guild = ? AND uid = ?`, [guildData.id, user.id])
+        if( !guildAccess || guildAccess.permission != 2 ){
+            return NextResponse.json({ success: false, message: 'У вас нету доступа к этой гильдии'}, { status: 400 });
+        }
+
+        const guildApplications : any = await query(`SELECT * FROM guilds_applications JOIN profiles ON fk_profile = profiles.id WHERE fk_guild = ? AND status = 'Рассматривается'`, [guildData.id])
+        return NextResponse.json({ success: true, data: guildApplications }, { status: 200 });
+    }catch (error: any){
+        return NextResponse.json({ success: false, message: 'Internal Server Error', error }, { status:500 })
+    }
+}
+
+export async function POST(request: NextRequest, {params}: { params: Promise<{ url: string }> }) {
+    const {url} = await params;
+    const data = await request.json();
+
+    const { error } = applicationSchema.validate(data);
+    if (error) {
+        return NextResponse.json({ success: false, message: 'Ошибка валидации', error: error.details }, { status: 400 });
+    }
+
+    const { about_user, why_this_guild } = data;
+
+    const authHeader = request.headers.get("authorization");
+    if (!authHeader) {
+        return NextResponse.json({success: false, message: 'Отсутствует заголовок авторизации'},{status: 401})
+    }
+    const token = authHeader.split(" ")[1];
+
+    try {
+        let response = await fetch("https://foxworld.ru/api/v1/users/me",{
+            method: "POST",
+            body: JSON.stringify({session_token: token}),
+        })
+
+        if ( !response.ok ){
+            const errorData = await response.json()
+            return NextResponse.json({success: false, message: 'Не удалось получить данные о пользователе', error: errorData || response.statusText},{status: response.status})
         }
 
         const json = await response.json()
@@ -48,16 +88,16 @@ export async function POST(request: NextRequest, {params}: { params: Promise<{ u
 
         const [guildData] : any = await query(`SELECT * FROM guilds WHERE url = ?`,[url])
         if( !guildData ){
-            return NextResponse.json({ success: false, message: 'Гильдия не найдена' },{status:404})
+            return NextResponse.json({ success: false, message: 'Гильдия не найдена' },{status: 400})
         }
 
         const [userGuild] : any = await query('SELECT permission FROM guilds_members WHERE uid = ? AND fk_guild = ?', [user.id, guildData.id])
         if ( userGuild ){
-            return NextResponse.json({ success: false, message: 'Вы уже состоите в этой гильдии' },{status:409})
+            return NextResponse.json({ success: false, message: 'Вы уже состоите в этой гильдии' },{status: 409})
         }
 
         await query('INSERT INTO guilds_applications (fk_guild, fk_profile, about_user, why_this_guild) VALUES (?, ?, ?, ?)', [guildData.id, user.id, about_user, why_this_guild])
-        return NextResponse.json({ success: true, message: "Заявка в гильдию отправлена" }, { status: 200 });
+        return NextResponse.json({ success: true, message: 'Заявка в гильдию отправлена' }, { status: 200 });
     }catch (error: any){
         return NextResponse.json({ success: false, message: 'Internal Server Error', error }, {status:500})
     }
