8cb17dafacce0175de8745233fe803c4c150b8bf
Найт <rodionvarzymov@outlook.com>
2025-02-14 09:02:59 +0300

    Улучшил отображение отсутствия заявок или участников в управлении игроками гильдии

--- app/me/guilds/[url]/users/page.tsx
+++ app/me/guilds/[url]/users/page.tsx
@@ -4,7 +4,7 @@ import {useEffect, useState} from "react";
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
 import {checkGuildAccess, getGuildApplications, getGuildUsers, getSession} from "@/app/actions/getInfo";
-import {LucideLoader, Pencil, Trash} from "lucide-react";
+import {LucideLoader, Pencil, SearchX, Trash} from "lucide-react";
 import ErrorMessage from "@/components/ui/error";
 
 type PageProps = {
@@ -134,7 +134,7 @@ export default function MyGuildMembers(props: PageProps) {
 
     const handleApplication = async (application: any, is_accepted: boolean) => {
         setIsLoading(true);
-        const user_id = application.fk_profiles;
+        const user_id = application.fk_profile;
         let status
 
         if ( is_accepted ) {
@@ -158,120 +158,116 @@ export default function MyGuildMembers(props: PageProps) {
             setIsLoading(false);
             return;
         }
-        setErrorType('success');
-        setErrorMessage(`Статус заявки успешно изменён на "${status}"`)
-        setIsLoading(false);
+        getGuildUsers(guildUrl).then((r)=>{
+            if (!r.success) {
+                setGuildUsers([]);
+                setErrorType('error');
+                setErrorMessage('Не удалось получить участников гильдии');
+                return
+            }
+            setGuildUsers(r.data);
+            setErrorType('success');
+            setErrorMessage(`Статус заявки успешно изменён на "${status}"`)
+            setIsLoading(false);
+        })
     }
     if (pageLoaded) {
         return (
             <div className="flex flex-col gap-2">
                 {errorMessage && <ErrorMessage message={errorMessage} onClose={handleClose} type={errorType} />}
-                <div className="h-full w-full">
-                    <div className="p-4 border-b border-gray-200 dark:border-gray-700 bg-neutral-100 dark:bg-neutral-800 rounded-lg shadow">
-                        <h2 className="text-xl font-semibold">Участники</h2>
-                    </div>
-                    <div className="my-2 grid sm:grid-cols-3 gap-2">
-                        {guildUsers.length != 0 ? guildUsers.map((user: any) => (
-                            <div key={user.uid} className="p-4 border-gray-200 dark:border-gray-700 bg-neutral-100 dark:bg-neutral-800 rounded-lg shadow flex flex-col gap-4">
-                                <div className="flex items-center gap-2 w-fit ">
-                                    <Image
-                                        src={"https://cravatar.eu/helmavatar/" + user.nickname + "/50.png"}
-                                        alt={user.nickname}
-                                        width={50}
-                                        height={50}
-                                        quality={100}
-                                        className='rounded-lg'
-                                    />
-                                    <h1 className='text-2xl'>{user.nickname}</h1>
-                                </div>
-                                <div>
-                                    <p>Уровень доступа: {user.permission}</p>
-                                    <p>Участник с {new Date(user.member_since).toLocaleString("ru-RU")}</p>
-                                </div>
-                                {user.permission != 2 && (
-                                    <div className="flex items-center gap-2">
-                                        <Button onClick={() => handleUpdateUser(user, 2)} disabled={isLoading} variant={"accent"} size={"sm"}>
-                                            {isLoading ? <><LucideLoader className="mr-2 animate-spin" /> Выполняю..</> : <><Pencil className="mr-2" />Повысить</>}
-                                        </Button>
-                                        <Button onClick={()=> handleDeleteUser(user)} disabled={isLoading} variant={"destructive"} size={"sm"}>
-                                            {isLoading ? <><LucideLoader className="mr-2 animate-spin" /> Выполняю..</> : <><Trash className="mr-2" />Исключить</>}
-                                        </Button>
+                { guildUsers.length != 0
+                    ?
+                        <div className="h-full w-full">
+                            <div className="p-4 border-b border-gray-200 dark:border-gray-700 bg-neutral-100 dark:bg-neutral-800 rounded-lg shadow">
+                                <h2 className="text-xl font-semibold">Участники</h2>
+                            </div>
+                            <div className="my-2 grid sm:grid-cols-3 gap-2">
+                                {guildUsers.map((user: any) => (
+                                    <div key={user.uid} className="p-4 border-gray-200 dark:border-gray-700 bg-neutral-100 dark:bg-neutral-800 rounded-lg shadow flex flex-col gap-4">
+                                        <div className="flex items-center gap-2 w-fit ">
+                                            <Image
+                                                src={"https://cravatar.eu/helmavatar/" + user.nickname + "/50.png"}
+                                                alt={user.nickname}
+                                                width={50}
+                                                height={50}
+                                                quality={100}
+                                                className='rounded-lg'
+                                            />
+                                            <h1 className='text-2xl'>{user.nickname}</h1>
+                                        </div>
+                                        <div>
+                                            <p>Уровень доступа: {user.permission}</p>
+                                            <p>Участник с {new Date(user.member_since).toLocaleString("ru-RU")}</p>
+                                        </div>
+                                        {user.permission != 2 && (
+                                            <div className="flex items-center gap-2">
+                                                <Button onClick={() => handleUpdateUser(user, 2)} disabled={isLoading} variant={"accent"} size={"sm"}>
+                                                    {isLoading ? <><LucideLoader className="mr-2 animate-spin" /> Выполняю..</> : <><Pencil className="mr-2" />Повысить</>}
+                                                </Button>
+                                                <Button onClick={()=> handleDeleteUser(user)} disabled={isLoading} variant={"destructive"} size={"sm"}>
+                                                    {isLoading ? <><LucideLoader className="mr-2 animate-spin" /> Выполняю..</> : <><Trash className="mr-2" />Исключить</>}
+                                                </Button>
+                                            </div>
+                                        )}
                                     </div>
-                                )}
+                                ))}
                             </div>
-                        )) :
-                          Array(3).fill(null).map((_, key) => ( // Render 5 skeleton items
-                            <div key={key} className="w-full bg-neutral-100 rounded-sm p-4 dark:bg-neutral-800 h-fit flex flex-col gap-4 animate-pulse">
-                              <div className="flex items-center gap-2 w-fit">
-                                <div className="rounded-lg bg-gray-300 dark:bg-gray-700 w-12 h-12"></div> {/* Avatar placeholder */}
-                                <div className="bg-gray-300 dark:bg-gray-700 w-24 h-6 rounded"></div> {/* Nickname placeholder */}
-                              </div>
-                              <div>
-                                <div className="bg-gray-300 dark:bg-gray-700 w-32 h-4 rounded mb-2"></div> {/* Permission placeholder */}
-                                <div className="bg-gray-300 dark:bg-gray-700 w-48 h-4 rounded"></div> {/* Member since placeholder */}
-                              </div>
-                              <div className="flex items-center gap-2">
-                                <div className="bg-gray-300 dark:bg-gray-700 w-20 h-8 rounded"></div> {/* Button placeholder */}
-                                <div className="bg-gray-300 dark:bg-gray-700 w-24 h-8 rounded"></div> {/* Button placeholder */}
-                              </div>
+                        </div>
+                    :
+                        <div className='sm:w-fit w-full bg-neutral-100 rounded-sm p-4 dark:bg-neutral-800 flex flex-col h-fit justify-between gap-2'>
+                            <div className=''>
+                                <SearchX className='h-20 w-20'/>
+                                <h1 className='text-3xl'>Участники не найдены</h1>
+                                <p>Попробуйте активнее агетировать о существовании своей гильдии</p>
                             </div>
-                          ))
-                        }
-                    </div>
-                </div>
-                <div className="h-full w-full">
-                    <div className="p-4 border-b border-gray-200 dark:border-gray-700 bg-neutral-100 dark:bg-neutral-800 rounded-lg shadow">
-                        <h2 className="text-xl font-semibold">Заявки</h2>
-                    </div>
-                    <div className="my-2 grid sm:grid-cols-3 gap-2">
-                        {guildApplications.length != 0  ? guildApplications.map((application: any) => (
-                            <div key={application.fk_profile} className="p-4 border-gray-200 dark:border-gray-700 bg-neutral-100 dark:bg-neutral-800 rounded-lg shadow flex flex-col gap-4">
-                                <div className="flex items-center gap-2 w-fit ">
-                                    <Image
-                                        src={"https://cravatar.eu/helmavatar/" + application.nick + "/50.png"}
-                                        alt={application.nick}
-                                        width={50}
-                                        height={50}
-                                        quality={100}
-                                        className='rounded-lg'
-                                    />
-                                    <h1 className='text-2xl'>{application.nick}</h1>
-                                </div>
-                                <div>
-                                    <p>О игроке: {application.about_user}</p>
-                                    <p>Почему решил вступить к вам: {application.why_this_guild}</p>
-                                    <p>Заявка создана {new Date(application.create_data).toLocaleString("ru-RU")}</p>
-                                </div>
-                                <div className="flex items-center gap-2">
-                                    <Button onClick={() => handleApplication(application, true)} disabled={isLoading} variant={"accent"} size={"sm"}>
-                                        {isLoading ? <><LucideLoader className="mr-2 animate-spin" /> Выполняю..</> : <><Pencil className="mr-2" />Принять</>}
-                                    </Button>
-                                    <Button onClick={()=> handleApplication(application, false)} disabled={isLoading} variant={"destructive"} size={"sm"}>
-                                        {isLoading ? <><LucideLoader className="mr-2 animate-spin" /> Выполняю..</> : <><Trash className="mr-2" />Отклонить</>}
-                                    </Button>
-                                </div>
+                        </div>
+                }
+                { guildApplications.length != 0
+                    ?
+                        <div className="h-full w-full">
+                            <div className="p-4 border-b border-gray-200 dark:border-gray-700 bg-neutral-100 dark:bg-neutral-800 rounded-lg shadow">
+                                <h2 className="text-xl font-semibold">Заявки</h2>
                             </div>
-                        ))
-                        :
-                          Array(3).fill(null).map((_, key) => ( // Render 5 skeleton items
-                            <div key={key} className="w-full bg-neutral-100 rounded-sm p-4 dark:bg-neutral-800 h-fit flex flex-col gap-4 animate-pulse">
-                              <div className="flex items-center gap-2 w-fit">
-                                <div className="rounded-lg bg-gray-300 dark:bg-gray-700 w-12 h-12"></div> {/* Avatar placeholder */}
-                                <div className="bg-gray-300 dark:bg-gray-700 w-24 h-6 rounded"></div> {/* Nickname placeholder */}
-                              </div>
-                              <div>
-                                <div className="bg-gray-300 dark:bg-gray-700 w-32 h-4 rounded mb-2"></div> {/* Permission placeholder */}
-                                <div className="bg-gray-300 dark:bg-gray-700 w-48 h-4 rounded"></div> {/* Member since placeholder */}
-                              </div>
-                              <div className="flex items-center gap-2">
-                                <div className="bg-gray-300 dark:bg-gray-700 w-20 h-8 rounded"></div> {/* Button placeholder */}
-                                <div className="bg-gray-300 dark:bg-gray-700 w-24 h-8 rounded"></div> {/* Button placeholder */}
-                              </div>
+                            <div className="my-2 grid sm:grid-cols-3 gap-2">
+                                {guildApplications.map((application: any) => (
+                                    <div key={application.fk_profile} className="p-4 border-gray-200 dark:border-gray-700 bg-neutral-100 dark:bg-neutral-800 rounded-lg shadow flex flex-col gap-4">
+                                        <div className="flex items-center gap-2 w-fit ">
+                                            <Image
+                                                src={"https://cravatar.eu/helmavatar/" + application.nick + "/50.png"}
+                                                alt={application.nick}
+                                                width={50}
+                                                height={50}
+                                                quality={100}
+                                                className='rounded-lg'
+                                            />
+                                            <h1 className='text-2xl'>{application.nick}</h1>
+                                        </div>
+                                        <div>
+                                            <p>О игроке: {application.about_user}</p>
+                                            <p>Почему решил вступить к вам: {application.why_this_guild}</p>
+                                            <p>Заявка создана {new Date(application.create_data).toLocaleString("ru-RU")}</p>
+                                        </div>
+                                        <div className="flex items-center gap-2">
+                                            <Button onClick={() => handleApplication(application, true)} disabled={isLoading} variant={"accent"} size={"sm"}>
+                                                {isLoading ? <><LucideLoader className="mr-2 animate-spin" /> Выполняю..</> : <><Pencil className="mr-2" />Принять</>}
+                                            </Button>
+                                            <Button onClick={()=> handleApplication(application, false)} disabled={isLoading} variant={"destructive"} size={"sm"}>
+                                                {isLoading ? <><LucideLoader className="mr-2 animate-spin" /> Выполняю..</> : <><Trash className="mr-2" />Отклонить</>}
+                                            </Button>
+                                        </div>
+                                    </div>
+                                ))}
                             </div>
-                          ))
-                        }
-                    </div>
-                </div>
+                        </div>
+                    :
+                        <div className='sm:w-fit w-full bg-neutral-100 rounded-sm p-4 dark:bg-neutral-800 flex flex-col h-fit justify-between gap-2'>
+                            <div className=''>
+                                <SearchX className='h-20 w-20'/>
+                                <h1 className='text-3xl'>Заявки не найдены</h1>
+                                <p>Попробуйте активнее агетировать о существовании своей гильдии</p>
+                            </div>
+                        </div>
+                }
             </div>
         );
     }
