9005d0ee915a5f7f33b7673c7f26b98f4a575d22
PWNight <rodionvarzymov@outlook.com>
2025-02-14 15:43:31 +0300

    Обновил футер

--- components/footer.tsx
+++ components/footer.tsx
@@ -19,7 +19,7 @@ export function Footer() {
                     />
                     <div className='flex flex-col'>
                         <p>FoxWorld © 2021 - 2025</p>
-                        <p>v1.0.0-beta2 (DEV-4 09.02.25)</p>
+                        <p>v1.0.0-beta2</p>
                     </div>
                 </div>
             </div>
