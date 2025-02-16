d3bc0bb3e781be1efb78ce619c1a52c334cc9e5d
Найт <rodionvarzymov@outlook.com>
2025-02-16 11:28:16 +0300

    Обновил футер

--- components/footer.tsx
+++ components/footer.tsx
@@ -19,7 +19,7 @@ export function Footer() {
                     />
                     <div className='flex flex-col'>
                         <p>FoxWorld © 2021 - 2025</p>
-                        <p>v1.0.0-beta2</p>
+                        <p>v1.0.0-beta2 (16.02.25)</p>
                     </div>
                 </div>
             </div>
