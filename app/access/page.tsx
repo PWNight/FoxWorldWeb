e9834d296d6c5ae6cd11d1f28fc2f9364ce6669c
Найт <rodionvarzymov@outlook.com>
2025-02-23 15:17:30 +0300

    Добавил форму верификации

---
+++ app/access/page.tsx
@@ -0,0 +1,103 @@
+"use client";
+import {useActionState, useEffect, useState} from "react";
+import {useRouter} from "next/navigation";
+import Link from "next/link";
+import {LucideLoader} from "lucide-react";
+import {getSession} from "@/app/actions/getInfo";
+import {verify_application} from "@/app/actions/applications";
+
+export default function AccessApplications() {
+    const [userData, setUserData] = useState(Object)
+    const [state, action, pending] = useActionState(verify_application, undefined);
+    const [pageLoaded, setPageLoaded] = useState(false);
+    const router = useRouter();
+
+    useEffect(() => {
+        getSession().then(async(user_r) => {
+            if ( !user_r.success ){
+                router.push('/login')
+                return
+            }
+            console.log(user_r.data.profile)
+            if ( user_r.data.profile.has_access ){
+                router.push('/me')
+                return
+            }
+
+            setUserData(user_r.data);
+            setPageLoaded(true);
+        })
+    }, [router]);
+
+    if ( pageLoaded ) {
+        return (
+            <div className="flex items-center flex-col justify-center w-full h-full">
+                <form className="text-sm w-auto mx-auto bg-neutral-100 dark:bg-neutral-800 rounded-xl p-10 text-gray-900 dark:text-gray-100" action={action}>
+                    <h1 className="text-3xl sm:text-4xl mb-5 select-none">Верификация</h1>
+                    <div className="mb-5 select-none">
+                        <label htmlFor="nickname" className="block mb-2 font-medium">Ваш никнейм</label>
+                        <input
+                            type="text"
+                            id="nickname"
+                            name="nickname"
+                            value={userData.profile.nick}
+                            readOnly={true}
+                            className="bg-gray-50 border border-gray-300 rounded-lg focus:ring-orange-300 focus:border-orange-400 block w-full p-2.5 dark:bg-neutral-800 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-orange-300 dark:focus:border-orange-400"
+                        />
+                        <p>Не ваш никнейм? Обратитесь в <Link href={'mailto:support@foxworld.ru'}>службу поддержки</Link></p>
+                        {state?.errors?.nickname && <p className="text-red-400 mt-1 mb-5">{state.errors.nickname}</p>}
+                    </div>
+                    <div className="mb-5 select-none">
+                        <label htmlFor="age" className="block mb-2 font-medium">Ваш возраст</label>
+                        <input
+                            id="age"
+                            type="number"
+                            name="age"
+                            className="mb-1 bg-gray-50 border border-gray-300 rounded-lg focus:ring-orange-300 focus:border-orange-400 block w-full p-2.5 dark:bg-neutral-800 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-orange-300 dark:focus:border-orange-400"
+                        />
+                        {state?.errors?.age && <p className="text-red-400 mt-1 mb-5">{state.errors.age}</p>}
+                    </div>
+                    <div className="mb-5 select-none">
+                        <label htmlFor="about" className="block mb-2 font-medium">Расскажите немного о вас</label>
+                        <input
+                            id="about"
+                            type="text"
+                            name="about"
+                            placeholder={'Чем увлекаетесь, что умеете'}
+                            className="mb-1 bg-gray-50 border border-gray-300 rounded-lg focus:ring-orange-300 focus:border-orange-400 block w-full p-2.5 dark:bg-neutral-800 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-orange-300 dark:focus:border-orange-400"
+                        />
+                        {state?.errors?.about && <p className="text-red-400 mt-1 mb-5">{state.errors.about}</p>}
+                    </div>
+                    <div className="mb-5 select-none">
+                        <label htmlFor="where_find" className="block mb-2 font-medium">Откуда узнали о проекте?</label>
+                        <input
+                            id="where_find"
+                            type="text"
+                            name="where_find"
+                            placeholder={'На мониторинге, в ТГ и т.п.'}
+                            className="mb-1 bg-gray-50 border border-gray-300 rounded-lg focus:ring-orange-300 focus:border-orange-400 block w-full p-2.5 dark:bg-neutral-800 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-orange-300 dark:focus:border-orange-400"
+                        />
+                        {state?.errors?.where_find && <p className="text-red-400 mt-1 mb-5">{state.errors.where_find}</p>}
+                    </div>
+                    <div className="mb-5 select-none">
+                        <label htmlFor="plans" className="block mb-2 font-medium">Чем планируете заняться на сервере?</label>
+                        <input
+                            id="plans"
+                            type="text"
+                            name="plans"
+                            placeholder={'Вступлю в гильдию, стану президентом и т.п.'}
+                            className="mb-1 bg-gray-50 border border-gray-300 rounded-lg focus:ring-orange-300 focus:border-orange-400 block w-full p-2.5 dark:bg-neutral-800 dark:border-gray-600 dark:placeholder-gray-400  dark:focus:ring-orange-300 dark:focus:border-orange-400"
+                        />
+                        {state?.errors?.plans && <p className="text-red-400 mt-1 mb-5">{state.errors.plans}</p>}
+                    </div>
+                    <div>
+                        <button type="submit"
+                                className="select-none text-white bg-[#F38F54] hover:bg-orange-500 focus:ring-4 focus:outline-hidden focus:ring-orange-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center flex items-center gap-1" disabled={pending}>{pending ? <><LucideLoader className="mr-2 animate-spin"/><p>Выполняю..</p></> : 'Отправить заявку'}
+                        </button>
+                        {state?.message && <p className="text-red-400 mt-1 mb-5">{state.message}</p>}
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
---
+++ app/api/v1/users/applications/route.ts
@@ -0,0 +1,138 @@
+import {NextRequest, NextResponse} from "next/server";
+import Joi from "joi";
+import {query} from "@/lib/mysql";
+
+const applicationSchema = Joi.object({
+    nickname: Joi.string().required(),
+    age: Joi.number().required(),
+    about: Joi.string().required(),
+    where_find: Joi.string().required(),
+    plans: Joi.string().required(),
+})
+
+export async function GET(request: NextRequest) {
+    const authHeader = request.headers.get("authorization");
+    if (!authHeader) {
+        return NextResponse.json({success: false, message: 'Отсутствует заголовок авторизации'},{status: 401})
+    }
+    const token = authHeader.split(" ")[1];
+
+    try {
+        let response = await fetch("https://foxworld.ru/api/v1/users/me",{
+            method: "GET",
+            headers: {"Authorization": `Bearer ${token}`}
+        })
+
+        if ( !response.ok ){
+            const errorData = await response.json()
+            return NextResponse.json({success: false, message: 'Не удалось получить данные о пользователе', error: errorData || response.statusText},{status: response.status})
+        }
+
+        const json = await response.json()
+        if( !json.success ){
+            return NextResponse.json({ success: false, message: json.message }, { status: 401 });
+        }
+
+        const user = json.profile;
+        //TODO: Implement admin check
+
+        const verifyApplications : any = await query(`SELECT * FROM verify_applications WHERE status = 'Рассматривается'`)
+        return NextResponse.json({ success: true, data: verifyApplications }, { status: 200 });
+    }catch (error: any){
+        return NextResponse.json({ success: false, message: 'Internal Server Error', error }, { status:500 })
+    }
+}
+
+export async function POST(request: NextRequest) {
+    const data = await request.json();
+    const { application_id, status, nickname, reason } = data;
+    if ( !application_id || !status || !nickname ) {
+        return NextResponse.json({ success: false, message: 'Ошибка валидации' }, { status: 400 });
+    }
+
+    const authHeader = request.headers.get("authorization");
+    if (!authHeader) {
+        return NextResponse.json({success: false, message: 'Отсутствует заголовок авторизации'},{status: 401})
+    }
+    const token = authHeader.split(" ")[1];
+
+    try {
+        let response = await fetch("https://foxworld.ru/api/v1/users/me",{
+            method: "GET",
+            headers: {"Authorization": `Bearer ${token}`}
+        })
+
+        if ( !response.ok ){
+            const errorData = await response.json()
+            return NextResponse.json({success: false, message: 'Не удалось получить данные о пользователе', error: errorData || response.statusText},{status: response.status})
+        }
+
+        const json = await response.json()
+        if( !json.success ){
+            return NextResponse.json({ success: false, message: json.message }, { status: 401 });
+        }
+        const user = json.profile;
+        //TODO: Implement admin check
+
+        const [verifyApplication] : any = await query('SELECT * FROM verify_applications WHERE id = ?',[application_id])
+        if ( !verifyApplication ){
+            return NextResponse.json({ success: false, message: 'Заявка не найдена' },{status: 400})
+        }
+
+        await query('UPDATE verify_applications SET status = ?, staff_uid = ? WHERE id = ?',[status, user.id, application_id])
+
+        if ( status == 'Принята' ) {
+            await query('UPDATE profiles SET has_access = 1 WHERE nick = ?', [nickname])
+        }else{
+            await query('UPDATE verify_applications SET reason = ? WHERE id = ?',[reason, application_id])
+        }
+
+        return NextResponse.json({ success: true, message: 'Заявка успешно обновлена' }, { status: 200 });
+    }catch (error: any){
+        return NextResponse.json({ success: false, message: 'Internal Server Error', error }, {status:500})
+    }
+}
+
+export async function PUT(request: NextRequest) {
+    const data = await request.json();
+
+    const { error } = applicationSchema.validate(data);
+    if (error) {
+        return NextResponse.json({ success: false, message: 'Ошибка валидации', error: error.details }, { status: 400 });
+    }
+
+    const { nickname, age, about, where_find, plans } = data;
+
+    const authHeader = request.headers.get("authorization");
+    if (!authHeader) {
+        return NextResponse.json({success: false, message: 'Отсутствует заголовок авторизации'},{status: 401})
+    }
+    const token = authHeader.split(" ")[1];
+
+    try {
+        let response = await fetch("https://foxworld.ru/api/v1/users/me",{
+            method: "GET",
+            headers: {"Authorization": `Bearer ${token}`}
+        })
+
+        if ( !response.ok ){
+            const errorData = await response.json()
+            return NextResponse.json({success: false, message: 'Не удалось получить данные о пользователе', error: errorData || response.statusText},{status: response.status})
+        }
+
+        const json = await response.json()
+        if( !json.success ){
+            return NextResponse.json({ success: false, message: json.message }, { status: 401 });
+        }
+
+        const [verifyApplication] : any = await query("SELECT * FROM verify_applications WHERE nickname = ? AND status = 'Рассматривается'", [nickname])
+        if ( verifyApplication ){
+            return NextResponse.json({ success: false, message: "Нельзя создать новую заявку, пока старая не рассмотрена" }, { status: 400 });
+        }
+
+        await query('INSERT INTO verify_applications (nickname, age, about, where_find, plans) VALUES (?, ?, ?, ?, ?)', [nickname, age, about, where_find, plans])
+        return NextResponse.json({ success: true, message: 'Заявка на верификацию отправлена' }, { status: 200 });
+    }catch (error: any){
+        return NextResponse.json({ success: false, message: 'Internal Server Error', error }, {status:500})
+    }
+}
