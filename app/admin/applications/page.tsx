69eb6f2549d1df4641838cd6ba90bcc6a4731c1f
Найт <rodionvarzymov@outlook.com>
2025-02-25 11:05:40 +0300

    Доработал страницу управления заявками

--- app/admin/applications/page.tsx
+++ app/admin/applications/page.tsx
@@ -2,10 +2,11 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import {getSession, getVerifyApplications} from "@/app/actions/getInfo";
-import MeSkelet from "@/components/skelets/me_skelet";
-import InDev from "@/components/indev";
-import {LucideCheck, LucideLoader, LucideX} from "lucide-react";
+import {LucideCheck, LucideLoader, LucideX, SearchX} from "lucide-react";
 import Image from "next/image";
+import ErrorMessage from "@/components/ui/notify-alert";
+import Link from "next/link";
+import {buttonVariants} from "@/components/ui/button";
 
 export default function VerifyApplications() {
     const [userData, setUserData] = useState(Object)
@@ -34,7 +35,8 @@ export default function VerifyApplications() {
 
             const verifyResponse = await getVerifyApplications(r.data.token)
             if ( !verifyResponse.success ){
-                //TODO: Implement error handler
+                setNotifyMessage(`Произошла ошибка при загрузке заявок игроков`)
+                setNotifyType('error')
                 return
             }
             setVerifyApplications(verifyResponse.data)
@@ -46,106 +48,118 @@ export default function VerifyApplications() {
         setNotifyMessage('')
     }
 
-    const handleUpdate = async(status: string, appId: number) => {
+    const handleUpdate = async(status: string, nickname: string, appId: number) => {
         setUpdating(appId)
+
         const response = await fetch("/api/v1/users/applications", {
             headers: {
                 "Authorization": `Bearer ${userData.token}`
             },
             method: "POST",
             body: JSON.stringify({
-                applicationId: appId,
-                status: status,
+                application_id: appId,
+                status,
+                nickname
             })
         })
-
         if ( !response.ok ) {
-            //TODO: Implement error handler
+            const errorData = await response.json()
+            console.log(errorData)
+            setNotifyMessage(`Произошла ошибка ${response.status} при обновлении заявки игрока`)
+            setNotifyType('error')
+            setUpdating(0);
             return;
-        }
-        const json = await response.json();
-        if ( !json.success ){
-            //TODO: Implement error handler
-            return
         }
 
         await getVerifyApplications(userData.token).then(async r => {
             if ( !r.success ){
-                //TODO: Implement error handler
+                setNotifyMessage(`Произошла ошибка при загрузке заявок игроков`)
+                setNotifyType('error')
                 return
             }
+            setNotifyMessage(`Заявка игрока успешно обновлена`)
+            setNotifyType('success')
             setVerifyApplications(r.data)
         })
+
     }
 
     return (
-        <div className=" p-4">
+        <div className="p-4">
+            { notifyMessage && <ErrorMessage message={notifyMessage} onClose={handleClose} type={notifyType} />}
             <h1 className="text-3xl font-bold mb-4">Управление заявками</h1>
-
             {!pageLoaded ? (
                 <div className="flex gap-2 text-xl">
                     <LucideLoader className="animate-spin" /> <p>Загружаю анкеты...</p>
                 </div>
-            ) : (
-                <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4">
-                    {verifyApplications.map((app:any) => (
-                        <div key={app.id} className="bg-white dark:bg-neutral-800 rounded-lg shadow p-6">
-                            <div className="flex flex-row gap-1 items-center mb-2">
-                                <Image
-                                    src={"https://minotar.net/helm/" + app.nickname + "/25.png"}
-                                    alt={app.nickname}
-                                    width={25}
-                                    height={25}
-                                    quality={100}
-                                    className={'rounded-md overflow-hidden'}
-                                />
-                                <h1 className={'text-lg font-medium'}>{app.nickname}</h1>
-                            </div>
-                            <div className="mb-2">
-                                <p className="text-gray-500 dark:text-gray-400">Возраст: {app.age}</p>
-                            </div>
-                            <div className="mb-2">
-                                <p className="text-gray-500 dark:text-gray-400">О себе: {app.about}</p>
-                            </div>
-                            <div className="mb-2">
-                                <p className="text-gray-500 dark:text-gray-400">Откуда узнал: {app.where_find}</p>
-                            </div>
-                            <div className="mb-2">
-                                <p className="text-gray-500 dark:text-gray-400">Планы: {app.plans}</p>
-                            </div>
-                            <div className="mb-4">
-                                <p className={`font-medium ${
-                                    app.status === 'approved' ? 'text-green-500' :
-                                        app.status === 'rejected' ? 'text-red-500' :
-                                            'text-yellow-500' // Pending or other statuses
-                                }`}>
-                                    Статус: {app.status}
-                                </p>
-                            </div>
-                            <div className="flex gap-2">
-                                {updating == app.id ? <p><LucideLoader className="animate-spin w-4 h-4"/> Выполняю</p> :
+            ) :
+                verifyApplications.length > 0 ? (
+                    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4">
+                        {verifyApplications.map((app:any) => (
+                            <div key={app.id} className="bg-white dark:bg-neutral-800 rounded-lg shadow p-4 w-fit">
+                                <div className="flex flex-row gap-1 items-center mb-2">
+                                    <Image
+                                        src={"https://minotar.net/helm/" + app.nickname + "/25.png"}
+                                        alt={app.nickname}
+                                        width={25}
+                                        height={25}
+                                        quality={100}
+                                        className={'rounded-md overflow-hidden'}
+                                    />
+                                    <h1 className={'text-lg font-medium'}>{app.nickname}</h1>
+                                </div>
+                                <div className="mb-2">
+                                    <p className="text-gray-500 dark:text-gray-400">Возраст: {app.age}</p>
+                                </div>
+                                <div className="mb-2">
+                                    <p className="text-gray-500 dark:text-gray-400">О себе: {app.about}</p>
+                                </div>
+                                <div className="mb-2">
+                                    <p className="text-gray-500 dark:text-gray-400">Откуда узнал: {app.where_find}</p>
+                                </div>
+                                <div className="mb-2">
+                                    <p className="text-gray-500 dark:text-gray-400">Планы: {app.plans}</p>
+                                </div>
+                                <div className="mb-4">
+                                    <p className={`font-medium ${
+                                        app.status === 'approved' ? 'text-green-500' :
+                                            app.status === 'rejected' ? 'text-red-500' :
+                                                'text-yellow-500' // Pending or other statuses
+                                    }`}>
+                                        Статус: {app.status}
+                                    </p>
+                                </div>
+                                <div className="flex gap-2">
                                     <button
-                                        onClick={() => handleUpdate('Принята',app.id)}
+                                        onClick={() => handleUpdate('Принята', app.nickname, app.id)}
                                         disabled={updating === app.id}
                                         className="bg-green-500 hover:bg-green-700 text-white py-1 px-2 rounded flex"
                                     >
-                                        <LucideCheck /> Принять
+                                        {updating == app.id ? <><LucideLoader className="mr-2 animate-spin" /> Выполняю..</> : <><LucideCheck className={'mr-2'}/>Принять</>}
                                     </button>
-                                }
-                                {updating == app.id ? <p><LucideLoader className="animate-spin w-4 h-4"/> Выполняю</p> :
                                     <button
-                                        onClick={() => handleUpdate('Отклонена',app.id)}
+                                        onClick={() => handleUpdate('Отклонена', app.nickname, app.id)}
                                         disabled={updating === app.id}
                                         className="bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded flex"
                                     >
-                                        <LucideX /> Отклонить
+                                        {updating == app.id ? <><LucideLoader className="mr-2 animate-spin" /> Выполняю..</> : <><LucideX className={'mr-2'}/>Отклонить</>}
                                     </button>
-                                }
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        ))}
                 </div>
-            )}
+                    ): (
+                    <div className='w-fit bg-neutral-100 rounded-sm p-4 dark:bg-neutral-800 flex flex-col h-fit justify-between gap-2'>
+                        { notifyMessage && <ErrorMessage message={notifyMessage} onClose={handleClose} type={notifyType} />}
+                        <div className=''>
+                            <SearchX className='h-20 w-20'/>
+                            <h1 className='text-3xl'>Заявки не найдены</h1>
+                            <p>Пока что здесь вы можете найти только заявки, на которые нужен ответ.</p>
+                            <p>В будущем вы сможете просматривать все заявки, которые есть в системе.</p>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     )
 }
