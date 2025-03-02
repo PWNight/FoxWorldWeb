1c0d907ef64b4d508c7acad22583110794b91fbe
PWNight <rodionvarzymov@outlook.com>
2025-03-02 18:43:15 +0300

    Убрал ненужный импорт

--- app/me/guilds/[url]/users/page.tsx
+++ app/me/guilds/[url]/users/page.tsx
@@ -4,7 +4,7 @@ import {useEffect, useState} from "react";
 import Image from "next/image";
 import {Button} from "@/components/ui/button";
 import {checkGuildAccess, getGuildApplications, getGuildUsers, getSession} from "@/app/actions/getInfo";
-import {Crown, LucideLoader, Pencil, SearchX, Trash} from "lucide-react";
+import {LucideLoader, Pencil, SearchX, Trash} from "lucide-react";
 import ErrorMessage from "@/components/ui/notify-alert";
 
 type PageProps = {
