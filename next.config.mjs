3c25c0cf2cb68f090d22299b5bbf03ee17310fe5
PWNight <rodionvarzymov@outlook.com>
2025-03-02 17:22:01 +0300

    Добавил поддержку http протокола для изображений

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "**", // Разрешает все домены для HTTPS
+      },
+      {
+        protocol: "http", // Опционально, если нужны HTTP-сайты
         hostname: "**",
       },
     ],
