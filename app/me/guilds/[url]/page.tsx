a654a33ad1efdbba9422ce55f68856eccdab0a26
Найт <rodionvarzymov@outlook.com>
2025-03-02 13:38:59 +0300

    Поменял код сервера на ссылку в управлении гильдии

--- app/me/guilds/[url]/page.tsx
+++ app/me/guilds/[url]/page.tsx
@@ -263,7 +263,7 @@ export default function MyGuild(props: PageProps) {
                                     <input
                                         type="text"
                                         id="discord_code"
-                                        placeholder="Введите код приглашения (например 2yyeWQ5unZ)"
+                                        placeholder="Введите ссылку на ваш сервер  (например https://discord.gg/2yyeWQ5unZ)"
                                         defaultValue={userGuild.discord_code}
                                         onChange={handleInputChange}
                                         className="w-full border border-gray-300 rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-orange-500 dark:bg-neutral-700 dark:border-gray-600 dark:text-white"
