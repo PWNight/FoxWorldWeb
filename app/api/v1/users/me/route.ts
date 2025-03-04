618e71ba92d3ddde31664925017f9ec041ceefbd
Найт <rodionvarzymov@outlook.com>
2025-03-04 14:30:02 +0300

    Добавил в /users/me вывод инфы по его дискорд аккаунту

--- app/api/v1/users/me/route.ts
+++ app/api/v1/users/me/route.ts
@@ -1,6 +1,7 @@
-import {permsQuery, query} from "@/lib/mysql";
+import {minecraftQuery, permsQuery, query} from "@/lib/mysql";
 import { decrypt } from "@/lib/session";
 import { NextRequest, NextResponse } from "next/server";
+const botToken = process.env.BOT_TOKEN
 
 async function checkToken(token: any){
     const data : any = await decrypt(token)
@@ -21,11 +22,24 @@ async function checkToken(token: any){
         if ( !user ) {
             return NextResponse.json({ success: false, message: 'Пользователь не найден, обратитесь к администратору площадки' }, { status: 404 });
         }
-
         const { premium_uuid, joined, last_seen } = user;
 
-        let [group] : any = await permsQuery("SELECT primary_group FROM luckperms_players WHERE username = ?", [profile.nick])
-        return NextResponse.json({ success: true, user: {premium_uuid, joined, last_seen}, profile, group: group.primary_group, token }, {status:200})
+        const [group] : any = await permsQuery("SELECT primary_group FROM luckperms_players WHERE username = ?", [profile.nick])
+        const [discord] : any = await minecraftQuery("SELECT discord FROM ds_accounts WHERE uuid = ?", [profile.fk_uuid]);
+
+
+        const res = await fetch(`https://discord.com/api/guilds/921483461016031263/members/${discord.discord}`, {
+            headers: {
+                "Authorization": `Bot ${botToken}`
+            }
+        })
+        const json = await res.json()
+
+        if (!res.ok) {
+            return NextResponse.json({ success: false, message: json.message }, {status:500})
+        }
+
+        return NextResponse.json({ success: true, user: {premium_uuid, joined, last_seen}, profile, group: group.primary_group, discord: json, token }, {status:200})
     }catch (error: any){
         return NextResponse.json({ success: false, message: 'Internal Server Error', error }, {status:500})
     }
@@ -38,9 +52,9 @@ export async function GET(request: NextRequest) {
             const session_token = request.cookies.get('s_token')
             if( session_token === undefined ){
                 return NextResponse.json({ success: false, message: "Отсутсвует заголовок авторизации" }, { status: 401 })
-            }else{
-                return await checkToken(session_token?.value)
             }
+
+            return await checkToken(session_token?.value)
         }
         return await checkToken(authHeader.split(" ")[1])
     }catch (error: any){
