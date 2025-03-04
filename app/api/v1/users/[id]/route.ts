80a9fb652fedf126a73425391d31d72de7409aa0
Найт <rodionvarzymov@outlook.com>
2025-03-04 14:37:46 +0300

    Обновил инфу о пользователе в /users/[id]

--- app/api/v1/users/[id]/route.ts
+++ app/api/v1/users/[id]/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
-import { query } from "@/lib/mysql";
+import {minecraftQuery, permsQuery, query} from "@/lib/mysql";
+const botToken = process.env.BOT_TOKEN
+
 
 export async function GET(request: NextRequest, {params}: { params: Promise<{ id: string }> }) {
     const {id} = await params;
@@ -11,9 +13,27 @@ export async function GET(request: NextRequest, {params}: { params: Promise<{ id
         }
 
         const [user] : any = await query('SELECT * FROM librepremium_data WHERE uuid = ?', [profile.fk_uuid])
+        if ( !user ) {
+            return NextResponse.json({ success: false, message: 'Пользователь не найден, обратитесь к администратору площадки' }, { status: 404 });
+        }
         const { premium_uuid, joined, last_seen } = user;
 
-        return NextResponse.json({ success: true, user: {premium_uuid, joined, last_seen}, profile }, {status:200})
+        const [group] : any = await permsQuery("SELECT primary_group FROM luckperms_players WHERE username = ?", [profile.nick])
+        const [discord] : any = await minecraftQuery("SELECT discord FROM ds_accounts WHERE uuid = ?", [profile.fk_uuid]);
+
+
+        const res = await fetch(`https://discord.com/api/guilds/921483461016031263/members/${discord.discord}`, {
+            headers: {
+                "Authorization": `Bot ${botToken}`
+            }
+        })
+        const json = await res.json()
+
+        if (!res.ok) {
+            return NextResponse.json({ success: false, message: json.message }, {status:500})
+        }
+
+        return NextResponse.json({ success: true, user: {premium_uuid, joined, last_seen}, profile, group: group.primary_group, discord: json }, {status:200})
     }catch (error: any){
         return NextResponse.json({ success: false, message: 'Internal Server Error', error }, {status:500})
     }
