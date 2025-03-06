ec9e0098ba97ce5be80ad09c615e0cde0788786a
PWNight <rodionvarzymov@outlook.com>
2025-03-07 00:25:57 +0300

    Разработать ф-ию для отправки любого сообщения в дс

--- lib/discord.ts
+++ lib/discord.ts
@@ -1,4 +1,34 @@
+import {NextResponse} from "next/server";
 
 export async function sendDiscordMessage(discordId: string, message: string) {
-  return false
+  const botToken = process.env.BOT_TOKEN;
+
+  let response = await fetch(`https://discord.com/api/users/@me/channels`,{
+    headers: {
+      "Authorization": `Bot ${botToken}`,
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({"recipient_id": discordId}),
+  })
+  const channels = await response.json()
+
+  if ( !response.ok ){
+    return { success: false, data: channels }
+  }
+
+  response = await fetch(`https://discord.com/api/channels/${channels.id}/messages`,{
+    headers: {
+      "Authorization": `Bot ${botToken}`,
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify({"content": "Hello!"}),
+  })
+  const json = await response.json()
+
+  if ( !response.ok ){
+    return { success: false, data: channels }
+  }
+  return { success: true }
 }
