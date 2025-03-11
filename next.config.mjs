1b45f093979db3195e894ab5fbaec9b2d86e5e11
PWNight <rodionvarzymov@outlook.com>
2025-03-11 23:28:34 +0300

    Убрал лишние переменные из .env

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  env: {
-    "SESSION_SECRET":"2n6!Ql,s;IqA|0|+2AD;Yvu;AIU/I%",
-    "BOT_TOKEN":"OTIxNDgyMzc3NTA1NjczMjY3.GmAaLD.5UovnILZWgaGO_5E-nTQT9hGr2HEW7Yplki190"
-  },
   images: {
     remotePatterns: [
       {
