dc5d5d0e038253e1d6961f078d94bb042efc1722
Найт <rodionvarzymov@outlook.com>
2025-03-11 13:58:52 +0300

    Обновил ссылку на онлайн карту

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -2,6 +2,6 @@
 
 export default function Map() {
     return (
-        <iframe src='http://135.181.126.159:25599' className='w-full h-full'></iframe>
+        <iframe src='https://foxworldmap.dynmap.xyz' className='w-full h-full'></iframe>
     );
 }
