bf75e63de38b589aaa03282d4dbfb3ec146d64ea
Найт <rodionvarzymov@outlook.com>
2025-03-11 17:13:30 +0300

    Выделил больше места под навигацию ЛК

--- app/me/layout.tsx
+++ app/me/layout.tsx
@@ -3,7 +3,7 @@ import {NavMe} from "@/components/navbar_me";
 
 export default function MeLayout({ children }: PropsWithChildren) {
   return (
-    <div className="grid sm:grid-cols-[300px_1fr] gap-6 mt-4 mb-4 sm:px-4 lg:w-[90%] w-full mx-auto">
+    <div className="grid sm:grid-cols-[350px_1fr] gap-6 mt-4 mb-4 sm:px-4 lg:w-[90%] w-full mx-auto">
         <NavMe/>
         {children}
     </div>
