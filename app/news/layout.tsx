0a0847953111083fc0713ecc150ac91dda37101d
PWNight <rodionvarzymov@outlook.com>
2025-03-18 00:01:18 +0300

    Убрал новости

--- app/news/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export default function BlogLayout({ children }: PropsWithChildren) {
-  return (
-    <div className="flex flex-col items-start justify-center pt-8 pb-10 lg:w-[95%] sm:[95%] md:[75%] mx-auto px-4">
-      {children}
-    </div>
-  );
-}
