4ba8df136f3a6f704e97b5a88644af7e48ec685a
Найт <rodionvarzymov@outlook.com>
2025-03-25 14:37:45 +0300

    Убрал лишний див в навигации ЛК

--- components/navbar_me.tsx
+++ components/navbar_me.tsx
@@ -3,73 +3,71 @@ import {Castle, House, Landmark, PaintbrushVertical, Scale, Store} from "lucide-
 
 export function NavMe() {
     return (
-        <div>
-            <div className="bg-neutral-100 rounded-lg shadow-sm dark:bg-neutral-800 p-4 sm:p-6">
-                <h1 className="text-xl sm:text-2xl font-semibold border-b border-neutral-200 dark:border-neutral-700 pb-3">
-                    Меню навигации
-                </h1>
-                <div className="mt-4 space-y-2">
-                    <SubAnchor
-                        key="/me"
-                        activeClassName="bg-orange-400 text-white hover:bg-orange-500"
-                        absolute
-                        className="flex items-center gap-2 w-full py-2 px-3 rounded-md sm:text-base hover:bg-orange-400 hover:text-white transition-all duration-200"
-                        href="/me"
-                    >
-                        <House className="w-5 h-5" />
-                        Главная страница
-                    </SubAnchor>
-                    <SubAnchor
-                        key="/me/customization"
-                        activeClassName="bg-orange-400 text-white hover:bg-orange-500"
-                        absolute
-                        className="flex items-center gap-2 w-full py-2 px-3 rounded-md sm:text-base hover:bg-orange-400 hover:text-white transition-all duration-200"
-                        href="/me/customization"
-                    >
-                        <PaintbrushVertical className="w-5 h-5" />
-                        Кастомизация
-                    </SubAnchor>
-                    <SubAnchor
-                        key="/me/donate"
-                        activeClassName="bg-orange-400 text-white hover:bg-orange-500"
-                        absolute
-                        className="flex items-center gap-2 w-full py-2 px-3 rounded-md sm:text-base hover:bg-orange-400 hover:text-white transition-all duration-200"
-                        href="/me/donate"
-                    >
-                        <Store className="w-5 h-5" />
-                        Управление покупками
-                    </SubAnchor>
-                    <SubAnchor
-                        key="/me/guilds"
-                        activeClassName="bg-orange-400 text-white hover:bg-orange-500"
-                        absolute
-                        className="flex items-center gap-2 w-full py-2 px-3 rounded-md sm:text-base hover:bg-orange-400 hover:text-white transition-all duration-200"
-                        href="/me/guilds"
-                    >
-                        <Castle className="w-5 h-5" />
-                        Управление гильдиями
-                    </SubAnchor>
-                    <SubAnchor
-                        key="/me/jurisdiction"
-                        activeClassName="bg-orange-400 text-white hover:bg-orange-500"
-                        absolute
-                        className="flex items-center gap-2 w-full py-2 px-3 rounded-md sm:text-base hover:bg-orange-400 hover:text-white transition-all duration-200"
-                        href="/me/jurisdiction"
-                    >
-                        <Scale className="w-5 h-5" />
-                        Судебная система
-                    </SubAnchor>
-                    <SubAnchor
-                        key="/me/bank"
-                        activeClassName="bg-orange-400 text-white hover:bg-orange-500"
-                        absolute
-                        className="flex items-center gap-2 w-full py-2 px-3 rounded-md sm:text-base hover:bg-orange-400 hover:text-white transition-all duration-200"
-                        href="/me/bank"
-                    >
-                        <Landmark className="w-5 h-5" />
-                        Банковская система
-                    </SubAnchor>
-                </div>
+        <div className="bg-neutral-100 rounded-lg shadow-sm dark:bg-neutral-800 p-4 sm:p-6">
+            <h1 className="text-xl sm:text-2xl font-semibold border-b border-neutral-200 dark:border-neutral-700 pb-3">
+                Меню навигации
+            </h1>
+            <div className="mt-4 space-y-2">
+                <SubAnchor
+                    key="/me"
+                    activeClassName="bg-orange-400 text-white hover:bg-orange-500"
+                    absolute
+                    className="flex items-center gap-2 w-full py-2 px-3 rounded-md sm:text-base hover:bg-orange-400 hover:text-white transition-all duration-200"
+                    href="/me"
+                >
+                    <House className="w-5 h-5" />
+                    Главная страница
+                </SubAnchor>
+                <SubAnchor
+                    key="/me/customization"
+                    activeClassName="bg-orange-400 text-white hover:bg-orange-500"
+                    absolute
+                    className="flex items-center gap-2 w-full py-2 px-3 rounded-md sm:text-base hover:bg-orange-400 hover:text-white transition-all duration-200"
+                    href="/me/customization"
+                >
+                    <PaintbrushVertical className="w-5 h-5" />
+                    Кастомизация
+                </SubAnchor>
+                <SubAnchor
+                    key="/me/donate"
+                    activeClassName="bg-orange-400 text-white hover:bg-orange-500"
+                    absolute
+                    className="flex items-center gap-2 w-full py-2 px-3 rounded-md sm:text-base hover:bg-orange-400 hover:text-white transition-all duration-200"
+                    href="/me/donate"
+                >
+                    <Store className="w-5 h-5" />
+                    Управление покупками
+                </SubAnchor>
+                <SubAnchor
+                    key="/me/guilds"
+                    activeClassName="bg-orange-400 text-white hover:bg-orange-500"
+                    absolute
+                    className="flex items-center gap-2 w-full py-2 px-3 rounded-md sm:text-base hover:bg-orange-400 hover:text-white transition-all duration-200"
+                    href="/me/guilds"
+                >
+                    <Castle className="w-5 h-5" />
+                    Управление гильдиями
+                </SubAnchor>
+                <SubAnchor
+                    key="/me/jurisdiction"
+                    activeClassName="bg-orange-400 text-white hover:bg-orange-500"
+                    absolute
+                    className="flex items-center gap-2 w-full py-2 px-3 rounded-md sm:text-base hover:bg-orange-400 hover:text-white transition-all duration-200"
+                    href="/me/jurisdiction"
+                >
+                    <Scale className="w-5 h-5" />
+                    Судебная система
+                </SubAnchor>
+                <SubAnchor
+                    key="/me/bank"
+                    activeClassName="bg-orange-400 text-white hover:bg-orange-500"
+                    absolute
+                    className="flex items-center gap-2 w-full py-2 px-3 rounded-md sm:text-base hover:bg-orange-400 hover:text-white transition-all duration-200"
+                    href="/me/bank"
+                >
+                    <Landmark className="w-5 h-5" />
+                    Банковская система
+                </SubAnchor>
             </div>
         </div>
     );
