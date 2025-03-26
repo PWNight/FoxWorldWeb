cef44758758b10d5b37080954293e44d5a8d0bb2
Найт <rodionvarzymov@outlook.com>
2025-03-26 16:40:38 +0300

    Исправил размер навигации по ЛК

--- app/me/layout.tsx
+++ app/me/layout.tsx
@@ -4,7 +4,9 @@ import {NavMe} from "@/components/navbar_me";
 export default function MeLayout({ children }: PropsWithChildren) {
   return (
     <div className="grid sm:grid-cols-[300px_1fr] gap-6 mt-4 mb-4 sm:px-4 lg:w-[95%] w-full mx-auto">
-        <NavMe/>
+        <div className='h-fit'>
+            <NavMe/>
+        </div>
         {children}
     </div>
   );
