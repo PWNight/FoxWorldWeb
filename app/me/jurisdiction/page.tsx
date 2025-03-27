39ba870c0c6ffc68c42813bf2bf5935b148d0039
Найт <rodionvarzymov@outlook.com>
2025-03-27 13:49:43 +0300

    Добавил заготовки новых страниц ЛК

---
+++ app/me/jurisdiction/page.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import InDev from "@/components/indev";
+import {getSession} from "@/app/actions/getDataHandlers";
+
+export default function MeBank() {
+    const [userData, setUserData] = useState(Object)
+    const router = useRouter()
+
+    useEffect(()=>{
+        getSession().then(async r => {
+            if ( !r.success ) {
+                router.push("/login?to=me/jurisdiction")
+                return
+            }
+            setUserData(r.data)
+        });
+        //TODO: Page loaded state update (in last async function)
+    },[router])
+
+    if(Object.keys(userData).length != 0){
+        return (
+            <div className="">
+                <InDev/>
+            </div>
+        )
+    }
+}
