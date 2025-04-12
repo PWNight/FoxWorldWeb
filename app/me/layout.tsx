90317f99638d5ea91d5faabe94979c3b10e6adab
Найт <rodionvarzymov@outlook.com>
2025-04-12 20:09:58 +0300

    Вынес проверку авторизации за остальные страницы в слой

--- app/me/layout.tsx
+++ app/me/layout.tsx
@@ -1,13 +1,46 @@
+"use client";
 import { PropsWithChildren } from "react";
-import {NavMe} from "@/components/navbar";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { NavMe } from "@/components/navbar";
+import { getSession } from "@/app/actions/getDataHandlers";
+import Loading from "@/components/loading";
+import { SessionContext, SessionData } from "@/context/SessionContext";
 
 export default function MeLayout({ children }: PropsWithChildren) {
-  return (
-    <div className="grid sm:grid-cols-[300px_1fr] gap-6 mt-4 mb-4 sm:px-4 lg:w-[95%] w-full mx-auto">
-        <div className='h-fit'>
-            <NavMe/>
-        </div>
-        {children}
-    </div>
-  );
+    const [isAuthorized, setIsAuthorized] = useState<boolean | null>(null);
+    const [session, setSession] = useState<SessionData | null>(null);
+    const router = useRouter();
+
+    useEffect(() => {
+        getSession().then((r) => {
+            if (!r.success) {
+                router.push("/login?to=me");
+                return;
+            }
+            setSession(r.data);
+            setIsAuthorized(true);
+        });
+    }, [router]);
+
+    if (isAuthorized === null) {
+        return (
+            <Loading
+                text={"Проверяем вашу сессию, пожалуйста, подождите.."}
+                color={"orange"}
+                className={"h-full w-full"}
+            />
+        );
+    }
+
+    return (
+        <SessionContext.Provider value={{ session, isAuthorized }}>
+            <div className="grid sm:grid-cols-[300px_1fr] gap-6 mt-4 mb-4 sm:px-4 lg:w-[95%] w-full mx-auto">
+                <div className="h-fit">
+                    <NavMe />
+                </div>
+                {children}
+            </div>
+        </SessionContext.Provider>
+    );
 }
